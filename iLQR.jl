c9c44e86b0acf95add881324c91aedec36c0a90a
Taylor Howell <thowthowthow@gmail.com>
2018-07-11 15:01:04 -0700

    Fix merge

--- iLQR.jl
+++ iLQR.jl
@@ -2,7 +2,12 @@ module iLQR
     using RigidBodyDynamics
     using ForwardDiff
 
+    export
+        Model,
+        Solver,
+        Objective
+
     include("model.jl")
     include("solver.jl")
-    include("ilqr.jl")
+    include("ilqr_algorithm.jl")
 end
