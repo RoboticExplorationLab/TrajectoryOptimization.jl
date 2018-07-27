21345a6d94d0ef7303e7341438ea386b4d4adf1f
Brian Jackson <bjack205@gmail.com>
2018-07-27 11:44:11 -0700

    Make Dynamics a Module
    Export tuples of model and objective for pendulums

--- dynamics.jl
+++ dynamics.jl
@@ -1,4 +1,6 @@
-include("model.jl")
+module Dynamics
+
+using iLQR.Model, iLQR.Objective
 
 export
     pendulum,
@@ -7,7 +9,7 @@ export
     ballonbeam,
     acrobot
 
-## Simple Pendulum
+"""Simple Pendulum"""
 # https://github.com/HarvardAgileRoboticsLab/unscented-dynamic-programming/blob/master/pendulum_dynamics.m
 function pendulum_dynamics(x,u)
     m = 1.
@@ -18,16 +20,61 @@ function pendulum_dynamics(x,u)
     g = 9.81
     return [x[2]; (u - m*g*lc*sin(x[1]) - b*x[2])];
 end
-pendulum = Model(pendulum_dynamics,2,1)
+model = Model(pendulum_dynamics,2,1)
 
-## Dubins car
+n = model.n; # dimensions of system
+m = model.m; # dimensions of control
+
+# initial conditions
+x0 = [0; 0];
+
+# goal
+xf = [pi; 0]; # (ie, swing up)
+
+# costs
+Q = 1e-3*eye(n);
+Qf = 100*eye(n);
+R = 1e-3*eye(m);
+
+# simulation
+tf = 5;
+
+# Set up problem
+obj = Objective(Q, R, Qf, tf, x0, xf)
+pendulum = [model,obj]
+
+
+"""Double Pendulum"""
+# Load URDF
+urdf_doublependulum = "urdf/doublependulum.urdf"
+model = Model(urdf_doublependulum)
+n = model.n; # dimensions of system
+m = model.m; # dimensions of control
+
+# initial and goal states
+x0 = [0.;0.;0.;0.]
+xf = [pi;0.;0.;0.]
+
+# costs
+Q = 0.0001*eye(4)
+Qf = 250.0*eye(4)
+R = 0.0001*eye(2)
+
+# simulation
+tf = 5.0
+
+obj = Objective(Q,R,Qf,tf,x0,xf)
+doublependulum = [model,obj]
+
+"""## Dubins car"""
 function dubins_dynamics(x,u)
     return [u[1]*cos(x[3]); u[1]*sin(x[3]); u[2]]
 end
 
 dubins = Model(dubins_dynamics,3,2)
 
-## Ball on beam
+
+"""Ball on beam"""
 function ballonbeam_dynamics(x,u)
     g = 9.81
     m1 = .35
@@ -47,13 +94,14 @@ end
 
 ballonbeam = Model(ballonbeam_dynamics,4,1)
 
-## Double Pendulum
-urdf_doublependulum = "urdf/doublependulum.urdf"
-doublependulum = Model(urdf_doublependulum)
+
 
 ## Acrobot
-doublependulum = Model(urdf_doublependulum,[0.;1.])
+acrobot = Model(urdf_doublependulum,[0.;1.])
 
 ## Cartpole / Inverted Pendulum
 urdf_cartpole = "urdf/cartpole.urdf"
 cartpole = Model(urdf_cartpole,[1.;0.])
+
+
+end
