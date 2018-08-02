babe2e654b00f91b4cb3bf949ca816dde397a68c
Brian Jackson <bjack205@gmail.com>
2018-08-02 09:59:11 -0700

    Test suite with simple pendulum

--- Tests/runtests.jl
+++ Tests/runtests.jl
@@ -1,26 +1,22 @@
-include("iLQR.jl")
+include("../iLQR.jl")
+include("../dynamics.jl")
 using Base.Test
 using BenchmarkTools
 using iLQR
 
-@testset "iLQR" begin
-    @testset "SimplePendulum" begin
-    # initialization
-    include("simple_pendulum.jl")
-    iLQR.solve(solver)
-    x,u = iLQR.solve(solver)
-    println(x[:,end], xf)
-    @test isapprox(x[:,end], xf, atol=1e-2)
-    end
 
-    @testset "Solver" begin
+@testset "Simple Pendulum" begin
     include("simple_pendulum.jl")
-
-    # initialization methods 
-    @test_nowarn iLQR.Solver(model, obj)
-    @test_nowarn iLQR.Solver(model, obj, dt=0.01)
-    @test_nowarn iLQR.Solver(model, obj, iLQR.rk4)
-    @test_nowarn iLQR.Solver(model, obj, iLQR.midpoint)
-    @test_nowarn iLQR.Solver(model, obj, iLQR.midpoint, dt = 0.1)
-    end
 end
+@testset "Constrained Objective" begin
+    include("objective_tests.jl")
+end
+
+
+"""
+# NEEDED TESTS:
+- integration schemes
+- in place dynamics
+- infeasible start
+- constraint generator
+"""
