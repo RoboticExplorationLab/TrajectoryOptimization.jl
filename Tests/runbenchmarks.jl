1ae5324e33f88e367ed0c3240ceff225ba2bcab6
Brian Jackson <bjack205@gmail.com>
2018-08-02 20:00:19 -0700

    Start modifying benchmarks

--- Tests/runbenchmarks.jl
+++ Tests/runbenchmarks.jl
@@ -4,9 +4,17 @@ import iLQR, iLQR.solve
 using BenchmarkTools
 using Dynamics
 
-solver = iLQR.Solver(Dynamics.pendulum...,dt=0.1)
-# @btime iLQR.solve(solver)
-# println(x[:,end])
+# Unconstrained
+model! = iLQR.Model(Dynamics.pendulum_dynamics!,2,1)
+obj = Dynamics.pendulum[2]
+obj = iLQR.ConstrainedObjective(obj, u_min=-2, u_max=2)
 
-solver = iLQR.Solver(Dynamics.doublependulum...,dt=0.1)
-@time x,u = solve(solver)
+
+opts = iLQR.SolverOptions()
+opts.inplace_dynamics = true
+opts.benchmark = true
+opts.verbose = false
+solver = iLQR.Solver(model!,obj,dt=0.1,opts=opts)
+U = ones(solver.model.m, solver.N-1)
+@time results = iLQR.solve(solver,U)
+@test norm(results.X[:,end]-obj.xf) < 1e-3
