e23ade63d08497e54c56cfed60493c8f7a5292f9
Brian Jackson <bjack205@gmail.com>
2018-08-04 09:26:03 -0700

    Organize files

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,28 @@ else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+include("../iLQR.jl")
+include("../dynamics.jl")
+using Base.Test
+using BenchmarkTools
+using iLQR
+
+
+@testset "Simple Pendulum" begin
+    include("simple_pendulum.jl")
+end
+@testset "Constrained Objective" begin
+    include("objective_tests.jl")
+end
+
+
+"""
+# NEEDED TESTS:
+- integration schemes
+- in place dynamics
+- infeasible start
+- constraint generator
+- state inequality constraints
+- asymmetric bounds (one bounded, another not)
+- all solve methods
+"""
