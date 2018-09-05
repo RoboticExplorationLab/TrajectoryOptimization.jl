47bfc3fcea6b26d77a45570d18810ba4690249c9
Brian Jackson <bjack205@gmail.com>
2018-09-05 09:41:56 -0700

    Add dircol results

--- src/results.jl
+++ src/results.jl
@@ -338,3 +338,54 @@ function check_multipliers(results,solver)
 
     return nothing
 end
+
+DiffFloat = Union{Float64,ForwardDiff.Dual}
+struct DircolResults <: SolverIterResults
+    Z::Vector{Float64}
+    X::SubArray{Float64}
+    U::SubArray{Float64}
+    fVal::Matrix{Float64}
+    X_::Matrix{Float64}
+    U_::Union{Matrix{Float64},SubArray{Float64}}
+    fVal_::Matrix{Float64}
+    A::Array{Float64,3}
+    B::Array{Float64,3}
+    weights::Vector{Float64}
+    # c_colloc::Matrix{Float64}
+    # ceq::Matrix{Float64}
+    # c::Matrix{Float64}
+    # DircolResults(Z,X,U,fVal,X_,U_,fVal_,A,B,weights) =
+    #     new(Z,X,U,fVal,X_,U_,fVal_,A,B,weights)
+end
+
+abstract type DircolMethod end
+
+type HermiteSimpson <: DircolMethod end
+
+
+function DircolResults(n::Int,m::Int,N::Int,method::Symbol)
+    N = convert_N(N,method)
+    Z = zeros(N*(n+m))
+    z = reshape(Z,n+m,N)
+    X = view(z,1:n,:)
+    U = view(z,n+1:n+m,:)
+    fVal = zeros(X)
+    if method == :hermite_simpson
+        X_ = zeros(n,2N-1)
+        U_ = zeros(m,2N-1)
+        fVal_ = zeros(X_)
+    elseif method == :midpoint
+        X_ = zeros(n,N-1)
+        U_ = view(U,:,1:N-1)
+        fVal_ = zeros(X_)
+    else
+        X_ = X
+        U_ = U
+        fVal_ = fVal
+    end
+    N_ = size(X_,2)
+    A = zeros(n,n,N_)
+    B = zeros(n,m,N_)
+    weights = get_weights(method,N_)
+    DircolResults(Z,X,U,fVal,X_,U_,fVal_,A,B,weights)
+end
