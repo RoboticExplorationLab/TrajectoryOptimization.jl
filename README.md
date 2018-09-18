be0279fc7b215b2fcf933c2eac659f38d934c37a
Taylor Howell <thowell@stanford.edu>
2018-09-18 16:34:54 -0700

    second order lambda update for zoh dynamic arrays

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 ![Build Status](https://travis-ci.org/RoboticExplorationLab/TrajectoryOptimization.jl.svg?branch=master)
 
-A package for solving constrained and unconstrained trajectory optimization problems in Julia. The package currently uses only iterative LQR (iLQR) techniques, a derivative of Differential Dynamic Programming (DDP).
+A package for solving constrained and unconstrained trajectory optimization problems in Julia.
+
+The package currently has the following solvers:
+* iLQR
+* Direct Collocation
 
 The package currently has the following capabilities:
 * Solve trajectory optimization problems with quadratic costs and nonlinear equality or inequality constraints (both stage and terminal)
 * Auto-differentiation of non-linear dynamics and constraint functions via ForwardDiff.jl
 * Generate dynamics directly from a URDF via RigidBodyDynamics.jl
 * Initialize the solver with an infeasible trajectory, allowing the user to input an initial guess for either states, controls, or both.
-
--- src/solver_options.jl
+++ src/solver_options.jl
@@ -62,6 +62,7 @@ mutable struct SolverOptions
     γ_no::Float64 # penalty update multiplier when μ should not be update, typically 1.0 (or 1.0 + ϵ)
     τ::Float64 # update term; 0 < τ < 1
     outer_loop_update::Symbol # type of outer loop update (default, uniform, uniform_time_step, individual)
+    λ_second_order_update::Bool # second order update for Lagrange multipliers once sqrt(cost tolerance | gradient) < desired tolerance
 
     "Regularization parameters"
     ρ_initial::Float64 # initial regularization
@@ -75,14 +76,14 @@ mutable struct SolverOptions
         c1=1.0e-8,c2=2.0,max_state_value=1.0e16,max_control_value=1.0e16,gradient_tolerance=1e-4,gradient_intermediate_tolerance=1e-4,cost_tolerance=1.0e-5,cost_intermediate_tolerance=1.0e-2,
         constraint_tolerance=1e-3,iterations=1000,iterations_outerloop=50,
         iterations_linesearch=10,infeasible_regularization=1e6,cache=false,
-        benchmark=false,solve_feasible=true,infeasible=false,unconstrained=false,resolve_feasible=true,λ_min=-1.0e16,λ_max=1.0e16,μ_max=1.0e16,μ1=1.0,γ=10.0,γ_no=1.0,τ=0.25,outer_loop_update=:uniform,
+        benchmark=false,solve_feasible=true,infeasible=false,unconstrained=false,resolve_feasible=true,λ_min=-1.0e16,λ_max=1.0e16,μ_max=1.0e16,μ1=1.0,γ=10.0,γ_no=1.0,τ=0.25,outer_loop_update=:uniform,λ_second_order_update=true,
         ρ_initial=1.0,ρ_factor=1.6,ρ_max=1.0e10,ρ_min=1e-6,use_static=true)
 
         new(square_root,verbose,c1,c2,max_state_value,max_control_value,gradient_tolerance,gradient_intermediate_tolerance,cost_tolerance,cost_intermediate_tolerance,
         constraint_tolerance,iterations,iterations_outerloop,
         iterations_linesearch,infeasible_regularization,cache,
         benchmark,solve_feasible,infeasible,unconstrained,resolve_feasible,
-        λ_min,λ_max,μ_max,μ1,γ,γ_no,τ,outer_loop_update,ρ_initial,ρ_factor,ρ_max,ρ_min,
+        λ_min,λ_max,μ_max,μ1,γ,γ_no,τ,outer_loop_update,λ_second_order_update,ρ_initial,ρ_factor,ρ_max,ρ_min,
         use_static)
     end
 end
