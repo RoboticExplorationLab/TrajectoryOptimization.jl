806331a426bbd479d0953db27ea268d9aebcccce
Brian Jackson <bjack205@gmail.com>
2018-12-15 14:50:28 -0800

    Colored benchmark comparison

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -71,10 +71,19 @@ function load_benchmark_history()
     suite_history, stats_history = BenchmarkTools.load(histfile)
 end
 
-function baseline_comparison(results, stats;f=BenchmarkTools.median)
+function baseline_comparison(results, stats; f=BenchmarkTools.median, styled=true)
     base_res, base_stats = get_baseline_benchmark()
-    r_comp = compare_results(results, base_res,f=f)
-    s_comp = compare_stats(stats, base_stats)
+    benchmark_comparison(results, stats, base_res, base_stats)
+end
+
+function baseline_comparison()
+    results = run(suite)
+    baseline_comparison(results, stats)
+end
+
+function benchmark_comparison(r1,s1,r2,s2; f=BenchmarkTools.median, styled=true)
+    r_comp = compare_results(r1, r2,f=f)
+    s_comp = compare_stats(s1, s2)
     if [r[1] for r in r_comp] == [s[1] for s in s_comp]
     end
     for (r,s) in zip(r_comp,s_comp)
@@ -83,7 +92,7 @@ function baseline_comparison(results, stats;f=BenchmarkTools.median)
             merge!(r[2], s[2])
         end
     end
-    return r_comp
+    print_comparison_table(r_comp, styled=styled)
 end
 
 function compare_results(r1::BenchmarkGroup, r2::BenchmarkGroup; f=BenchmarkTools.median)
@@ -100,15 +109,26 @@ function compare_results(r1::BenchmarkGroup, r2::BenchmarkGroup; f=BenchmarkTool
         vals = [f(r1[run]), f(r2[run])]
         t = j[run]
         stats_comp = Dict()
-        stats_comp["time"] =  join([prettytime(val.time) for val in vals], " ← ")
-        stats_comp["gctime"] = join([prettytime(val.gctime) for val in vals], " ← ") * " (" * prettytime(vals[1].gctime - vals[2].gctime) * ")"
-        stats_comp["memory"] = join([prettymemory(val.memory) for val in vals], " ← ")
-        stats_comp["allocs"] = join([format(val.allocs, autoscale=:metric, precision=2) for val in vals], " ← ") * " (" * format(vals[1].allocs - vals[2].allocs, signed=true, autoscale=:metric, precision=3) * ")"
-        stats_comp["time"] *= " (" * prettydiff(time(ratio(t))) * ")"
-        stats_comp["memory"] *= " (" * prettydiff(memory(ratio(t))) * ")"
+        stats_comp["time"] =  (join([prettytime(val.time) for val in vals], " ← ") * " (" * prettydiff(time(ratio(t))) * ")", t.time)
+        stats_comp["memory"] = (join([prettymemory(val.memory) for val in vals], " ← ") * " (" * prettydiff(memory(ratio(t))) * ")", t.memory)
+        stats_comp["gctime"] = (join([prettytime(val.gctime) for val in vals], " ← ") * " (" * prettytime(vals[1].gctime - vals[2].gctime) * ")",
+            check_improvement(vals[1].gctime, vals[2].gctime, Inf))
+        stats_comp["allocs"] = (join([format(val.allocs, autoscale=:metric, precision=2) for val in vals], " ← ") * " (" * format(vals[1].allocs - vals[2].allocs, signed=true, autoscale=:metric, precision=3) * ")",
+            check_improvement(vals[1].allocs, vals[2].allocs))
         push!(comp, (run, stats_comp))
     end
     return sort(comp)
+end
+
+function check_improvement(v1,v2,tol=0.05)
+    diff = v1-v2
+    if diff > tol*v2
+        return :regression
+    elseif diff < -tol*v2
+        return :improvement
+    else
+        return :invariant
+    end
 end
 
 
@@ -133,7 +153,8 @@ function compare_stats(s1::BenchmarkGroup, s2::BenchmarkGroup)
             v1 = s1[run][stat]
             v2 = s2[run][stat]
             diff = v1 - v2
-            stat_comp[stat] = trim_entry(v1, width, pad=false, positivespace=false) * " ← " * trim_entry(v2, width, pad=false, positivespace=false) * " (" * trim_entry(diff, width, pad=false, positivespace=false, signed=true) * ")"
+            str = trim_entry(v1, width, pad=false, positivespace=false) * " ← " * trim_entry(v2, width, pad=false, positivespace=false) * " (" * trim_entry(diff, width, pad=false, positivespace=false, signed=true) * ")"
+            stat_comp[stat] = (str, check_improvement(v1,v2))
         end
         push!(comp,(run, stat_comp))
     end
@@ -154,64 +175,49 @@ function loop_groups(s1::BenchmarkGroup, s2::BenchmarkGroup)
 end
 
 
-r_comp = baseline_comparison(results, stats)
+function print_comparison_table(r_comp::Vector{Any}; styled=true)
+    cols = ["time","memory","allocs","iterations","major iterations","c_max","cost"]
 
-stats["dubinscar"]["parallel park"]["constrained"]
-id, vals = r_comp[1]
-cols = ["time","memory","allocs","iterations","major iterations","c_max","cost"]
+    ids = [join(run[1], ", ") for run in r_comp]
+    change = [[run[2][key][2] for key in cols] for run in r_comp]
+    change = hcat(change...)
+    table = [[rpad(run[2][key][1],20) for key in cols] for run in r_comp]
+    table = hcat(table...)
+    table = vcat(reshape(ids,1,length(ids)),table)
 
-ids = [join(run[1], ", ") for run in r_comp]
-# pushfirst!(ids, "IDs")
-table = [[rpad(run[2][key],20) for key in cols] for run in r_comp]
-table = hcat(table...)
-table = vcat(reshape(ids,1,length(ids)),table)
+    table = hcat(["IDs"; cols], table)
+    col_widths = maximum(length.(table), dims=2)
+    n_stats,n_runs = size(table)
+    for j = 1:n_runs, i=1:n_stats
+        table[i,j] = rpad(table[i,j],col_widths[i])
+    end
 
-# table = hcat(["IDs"; cols], table)
-col_widths = maximum(length.(table), dims=2)
-n_stats,n_runs = size(table)
-for j = 1:n_runs, i=1:n_stats
-    table[i,j] = rpad(table[i,j],col_widths[i])
+    header = "| " * join(table[:,1]," | ") * " |";
+    hline = "|" * join(["-"^(width+2) for width in col_widths],"|") * "|";
+
+    if styled
+        printstyled(header,bold=true); println();
+        println(hline)
+        colormap = Dict(:invariant=>:grey, :regression=>:red, :improvement=>:green)
+        for i = 2:n_runs
+            print("| ")
+            printstyled(table[1,i],bold=true)
+            for j = 2:n_stats
+                print(" | ")
+                printstyled(table[j,i],color=colormap[change[j-1,i-1]])
+            end
+            println(" |")
+        end
+    else
+        println(header)
+        println(hline)
+        for i = 2:n_runs
+            println("| " * join(table[:,i]," | ") * " |")
+        end
+    end
 end
 
 
-header = ["IDs"; cols]
-hline = "|" * join(["-"^(width+2) for width in col_widths],"|") * "|";
-print(join(header," | "))
-print(hline)
-for i = 2:n_runs
-    println("| " * join(table[:,i]," | ") * " |")
-end
-j
-r_comp = compare_results(results, results2)
-
-compare_stats(stats, stats_base)
-
-df = DataFrame()
-df.time = [r[2]["time"] for r in r_comp]
-df.memory = [r[2]["memory"] for r in r_comp]
-df.allocs = [r[2]["allocs"] for r in r_comp]
-df.gctime = [r[2]["gctime"] for r in r_comp]
-df.iterations = [r[2]["iterations"] for r in r_comp]
-df.major_iterations = [r[2]["time"] for r in r_comp]
-df.max_constraint = [r[2]["c_max"] for r in r_comp]
-
-
-
-export_markdown
-
-leaves(j)
-k = keys(stats["dubinscar"]["parallel park"]["constrained"])
-collect(k)
-k1 = "major iterations"
-k1 in k
-stats["dubinscar"]["parallel park"]["constrained"]["major iterations"]
-for s in
-    @show s[2]
-end
-stats[@tagged "stats"]
-
-t = j[1][2]
-prettyjudgetime(t)
 
 
 date = today()
@@ -226,3 +232,5 @@ loadparams!(suite, BenchmarkTools.load(paramsfile)[1])
 
 # Rename for PkgBenchmark.jl
 SUITE = suite
+
+# baseline_comparison(results, stats)
