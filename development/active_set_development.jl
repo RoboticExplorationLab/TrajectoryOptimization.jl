69a87b8a689cff04b9711cb6cd1ae881e197cad0
Taylor Howell <thowell@stanford.edu>
2018-12-16 22:01:36 -0800

    more active set method development

--- development/active_set_development.jl
+++ development/active_set_development.jl
@@ -26,19 +26,15 @@ results1 = init_results(solver1,X0,U0)
 rollout!(results1,solver1)
 calculate_jacobians!(results1, solver1)
 
-p,pI,pE = get_num_constraints(solver1)
-
-idx = Array(1:p)
-
-p_idx = idx[results1.active_set[1]]
+_backwardpass_active_set!(results1,solver1)
 
 a = 1
-
 function _backwardpass_active_set!(res::SolverVectorResults,solver::Solver)
     n,m,N = get_sizes(solver)
     m̄,mm = get_num_controls(solver)
     p,pI,pE = get_num_constraints(solver)
     pN = n
+
     W = solver.obj.Q; R = solver.obj.R; Wf = solver.obj.Qf; xf = solver.obj.xf
 
     solver.opts.minimum_time ? R_minimum_time = solver.opts.R_minimum_time : nothing
@@ -58,7 +54,6 @@ function _backwardpass_active_set!(res::SolverVectorResults,solver::Solver)
     u_idx = idx[n+1:n+m]
     ū_idx = idx[n+1:n+mm]
 
-    p_idx = Array(1:p)
 
     S = [k != N ? zeros(n+p,n+p) : zeros(n+pN,n+pN) for k = 1:N]
     s = [k != N ? zeros(n+p) : zeros(n+pN) for k = 1:N]
@@ -66,13 +61,20 @@ function _backwardpass_active_set!(res::SolverVectorResults,solver::Solver)
     # Boundary Conditions
     C = res.C; Iμ = res.Iμ; λ = res.λ; CxN = res.Cx_N
 
-    S[N][1:n,1:n] = Wf + CxN'*res.IμN*CxN
-    S[N][1:n,n+1:n+pN] = Cx_N'
-    S[N][n+1:n+pN,1:n] = Cx_N
-    S[N][n+1:n+pN,n+1:n+pN] = zeros(pN,pN)
+    Sxx = Wf + CxN'*res.IμN*CxN
+    Sxλ = CxN'
+    Sλλ = zeros(pN,pN)
 
-    s[N][1:n] = Wf*(X[N] - xf) + CxN'*(res.IμN*res.CN + res.λN)
-    s[N][n+1:n+pN] = res.CN
+    Sx = Wf*(X[N] - xf) + CxN'*(res.IμN*res.CN + res.λN)
+    Sλ = res.CN
+
+    S[N][1:n,1:n] = Sxx
+    S[N][1:n,n+1:n+pN] = Sxλ
+    S[N][n+1:n+pN,1:n] = Sxλ'
+    S[N][n+1:n+pN,n+1:n+pN] = Sλλ
+
+    s[N][1:n] = Sx
+    s[N][n+1:n+pN] = Sλ
 
     # Initialize expected change in cost-to-go
     Δv = zeros(2)
@@ -133,14 +135,18 @@ function _backwardpass_active_set!(res::SolverVectorResults,solver::Solver)
         end
 
         # Constraints
-        if res isa ConstrainedIterResults
-            Cx, Cu = res.Cx[k], res.Cu[k]
-            Lx += Cx'*(Iμ[k]*C[k] + λ[k])
-            Lu += Cu'*(Iμ[k]*C[k] + λ[k])
-            Lxx += Cx'*Iμ[k]*Cx
-            Luu += Cu'*Iμ[k]*Cu
-            Lux += Cu'*Iμ[k]*Cx
-        end
+        Cx, Cu = res.Cx[k], res.Cu[k]
+        Lx += Cx'*(Iμ[k]*C[k] + λ[k])
+        Lu += Cu'*(Iμ[k]*C[k] + λ[k])
+        Lxx += Cx'*Iμ[k]*Cx
+        Luu += Cu'*Iμ[k]*Cu
+        Lux += Cu'*Iμ[k]*Cx
+
+        # multiplier expansion terms
+        Lλ = res.C[k]
+        Lxλ = res.Cx[k]'
+        Lλλ = zeros(p,p)
+        Lλu = res.Cu[k]
 
         # Compute gradients of the dynamics
         fdx, fdu = res.fdx[k], res.fdu[k]
@@ -152,17 +158,11 @@ function _backwardpass_active_set!(res::SolverVectorResults,solver::Solver)
         Quu = Luu + fdu'*S[k+1][1:n,1:n]*fdu
         Qux = Lux + fdu'*S[k+1][1:n,1:n]*fdx
 
-        if solver.opts.regularization_type == :state
-            Luu_reg = Luu + res.ρ[1]*fdu'*fdu
-            Lux_reg = Lux + res.ρ[1]*fdu'*fdx
-            Quu_reg = Quu + res.ρ[1]*fdu'*fdu
-            Qux_reg = Qux + res.ρ[1]*fdu'*fdx
-        elseif solver.opts.regularization_type == :control
-            Quu_reg = Quu + res.ρ[1]*I
-            Qux_reg = Qux
-            Luu_reg = Luu + res.ρ[1]*I
-            Lux_reg = Lux
-        end
+        # regularization
+        Quu_reg = Quu + res.ρ[1]*I
+        Qux_reg = Qux
+        Luu_reg = Luu + res.ρ[1]*I
+        Lux_reg = Lux
 
         # Regularization
         if !isposdef(Hermitian(Array(Quu_reg)))  # need to wrap Array since isposdef doesn't work for static arrays
@@ -176,92 +176,117 @@ function _backwardpass_active_set!(res::SolverVectorResults,solver::Solver)
             continue
         end
 
-        # linear dynamics for cost-to-go
+        # linearized dynamics for cost-to-go
         S̄xx = fdx'*Sxx*fdx
         S̄xλ = fdx'*Sxλ
         S̄xu = fdx'*Sxx*fdu
-        S̄λx = Sλx*fdx
         S̄λλ = Sλλ
-        S̄λu = Sλx*fdu
-        S̄ux = fdu'*Sxx*fdx
-        S̄uλ = fdu'*Sxλ
+        S̄λu = Sxλ'*fdu
         S̄uu = fdu'*Sxx*fdu
 
         S̄x = fdx'*Sx
         S̄λ = Sλ
         S̄u = fdu'*Sx
 
+        # get active set constraints and Jacobians
+        active_set_idx_1 = res.active_set[k] # active set indices at index k
+        k != N-1 ? active_set_idx_2 = res.active_set[k+1] : active_set_idx_2 = ones(Bool,pN) # active set indices at index k+1
+
+        c_active_set_1 = res.C[k][active_set_idx_1] # active set constraints at index k
+        k != N-1 ? c_active_set_2 = res.C[k+1][active_set_idx_2] : c_active_set_2 = res.CN[active_set_idx_2] # active set constraints at index k+1
+        c_set_1 = res.C[k]
+        k != N-1 ? c_set_2 = res.C[k+1] : c_set_2 = res.CN
+
+        cx_active_set_1 = res.Cx[k][active_set_idx_1,:] # active set Jacobians at index k
+        cx_active_set_2 = S̄xλ[:,active_set_idx_2]' # active set Jacobians at index k+1
+        cx_set_1 = res.Cx[k]
+        cx_set_2 = S̄xλ'
+
+        cu_active_set_1 = res.Cu[k][active_set_idx_1,:] # active set Jacobians at index k
+        cu_active_set_2 = S̄λu[active_set_idx_2,:] # active set Jacobians at index k+1
+        cu_set_1 = res.Cu[k]
+        cu_set_2 = S̄λu
+
         # calculate multiplier gains
-        res.Kλ[k+1] = -(S̄λu*(Quu_reg\S̄λu'))\(S̄λu*(Quu_reg\Qux_reg) - S̄λx)
-        res.Mλ[k+1] = -(S̄λu*(Quu_reg\S̄λu'))\(S̄λu*(Quu_reg\res.Cu[k]'))
-        res.dλ[k+1] = -(S̄λu*(Quu_reg\S̄λu'))\(S̄λu*(Quu_reg\Qu) - res.C[k+1])
+        tmp = cu_active_set_2*(Quu_reg\cu_active_set_2')
+
+        res.Kλ[k+1][active_set_idx_2,:] = -tmp\(cu_active_set_2*(Quu_reg\Qux_reg) - cx_active_set_2)
+        res.Mλ[k+1][active_set_idx_2,active_set_idx_1] = -tmp\(cu_active_set_2*(Quu_reg\cu_active_set_1'))
+        res.dλ[k+1][active_set_idx_2] = -tmp\(cu_active_set_2*(Quu_reg\Qu) - c_active_set_2)
+
+        # reset inactive gains to zero (this may be unnecessary)
+        res.Kλ[k+1][.!active_set_idx_2,:] .= 0.
+        res.Mλ[k+1][.!active_set_idx_2,.!active_set_idx_1] .= 0.
+        res.dλ[k+1][.!active_set_idx_2] .= 0.
 
         if k != 1
             # calculate control gains
-            res.K[k] = -Quu_reg\(Qux_reg + S̄λu'*res.Kλ[k+1])
-            res.M[k] = -Quu_reg\(res.Cu[k] + S̄λu'*res.Mλ[k+1])
-            res.d[k] = -Quu_reg\(Qu + S̄λu'*res.dλ[k+1])
+            res.K[k] = -Quu_reg\(Qux_reg + cu_set_2'*res.Kλ[k+1])
+            res.M[k] = -Quu_reg\(cu_set_1' + cu_set_2'*res.Mλ[k+1])
+            res.d[k] = -Quu_reg\(Qu + cu_set_2'*res.dλ[k+1])
         else
-            res.Kλ[1] = -(res.Cu[k]*(Quu_reg\(res.Cu[k]' + S̄λu*res.Mλ[k+1])))\(res.Cu[k]*(Quu_reg\(Qux_reg + S̄λu*res.Kλ[k+1])) - S̄λx)
-            # res.Mλ[1] = -()\()
-            res.dλ[1] = -(res.Cu[k]*(Quu_reg\(res.Cu[k]' + S̄λu*res.Mλ[k+1])))\(res.Cu[k]*(Quu_reg\(Qu + S̄λu*res.dλ[k+1])) - res.C[k+1])
+            # calculate final multiplier gains
+            tmp = cu_active_set_1*(Quu_reg\(cu_active_set_1' + cu_active_set_2'*res.Mλ[k+1][active_set_idx_2,active_set_idx_1]))
+            res.Kλ[1][active_set_idx_1,:] = -tmp\(cu_active_set_1*(Quu_reg\(Qux_reg + cu_active_set_2'*res.Kλ[k+1][active_set_idx_2,:])) - cx_active_set_1)
+            res.dλ[1][active_set_idx_1] = -tmp\(cu_active_set_1*(Quu_reg\(Qu + cu_active_set_2'*res.dλ[k+1][active_set_idx_2])) - c_active_set_1)
 
-            res.K[1] = -Quu_reg\(Qux_reg + res.Cu[k]*res.Kλ[1] + S̄λu'*res.Kλ[k+1] + S̄λu'*res.Mλ[k+1]*res.Kλ[1])
-            # res.M[1] = -()\()
-            res.d[1] = -Quu_reg\(Qu + res.Cu[k]*res.dλ[1] + S̄λu'*res.dλ[k+1] + S̄λu'*res.Mλ[k+1]*res.dλ[1])
+            # reset inactive gains to zero (this may be unnecessary)
+            res.Kλ[1][.!active_set_idx_1,:] .= 0.
+            res.dλ[1][.!active_set_idx_1] .= 0.
+
+            # calculate final control gains
+            res.K[1] = -Quu_reg\(Qux_reg + cu_set_1'*res.Kλ[1] + cu_set_2'*res.Kλ[k+1] + cu_set_2'*res.Mλ[k+1]*res.Kλ[1])
+            res.d[1] = -Quu_reg\(Qu + cu_set_1'*res.dλ[1] + cu_set_2'*res.dλ[k+1] + cu_set_2'*res.Mλ[k+1]*res.dλ[1])
         end
 
         # plug in multiplier equation
-        Ŝxx = S̄xx + S̄xλ*res.Kλ[k+1] + res.Kλ[k+1]'*S̄xλ + res.Kλ[k+1]'*S̄λλ*res.Kλ[k+1]
+        Ŝxx = S̄xx + S̄xλ*res.Kλ[k+1] + res.Kλ[k+1]'*S̄xλ' + res.Kλ[k+1]'*S̄λλ*res.Kλ[k+1]
         Ŝxλ = S̄xλ*res.Mλ[k+1] + res.Kλ[k+1]'*S̄λλ*res.Mλ[k+1]
         Ŝxu = res.Kλ[k+1]'*S̄λu + S̄xu
-        Ŝλx = res.Mλ[k+1]'*S̄λx + res.Mλ[k+1]'*S̄λλ*res.Kλ[k+1]
         Ŝλλ = res.Mλ[k+1]'*S̄λλ*res.Mλ[k+1]
         Ŝλu = res.Mλ[k+1]'*S̄λu
-        Ŝux = S̄ux + S̄uλ*res.Kλ[k+1]
-        Ŝuλ = S̄uλ*res.Mλ[k+1]
         Ŝuu = S̄uu
 
-        Ŝx = res.dλ[k+1]'*S̄λx + res.dλ[k+1]'*S̄λλ*res.Kλ[k+1] + S̄x + S̄λ'*res.Kλ[k+1]
-        Ŝλ = res.dλ[k+1]'*S̄λλ*res.Mλ[k+1] + S̄λ'*res.Mλ[k+1]
-        Ŝu = res.dλ[k+1]'*S̄λu + S̄u'
+        Ŝx = S̄xλ*res.dλ[k+1] + res.Kλ[k+1]'*S̄λλ*res.dλ[k+1] + S̄x + res.Kλ[k+1]'*S̄λ
+        Ŝλ = res.Mλ[k+1]'*S̄λλ*res.dλ[k+1] + res.Mλ[k+1]'*S̄λ
+        Ŝu = S̄λu'*res.dλ[k+1] + S̄u
 
         Δv[1] += res.dλ[k+1]'*S̄λ
         Δv[2] += 0.5*res.dλ[k+1]'*S̄λλ*res.dλ[k+1]
 
         # calculate cost-to-go
-        Sxx = (Lxx + Ŝxx) + (Lxu + Ŝxu)*res.K[k] + res.K[k]'*(Lux + Ŝux) + res.K[k]'*(Luu + Ŝuu)*res.K[k]
-        Sxλ = (Lxu + Ŝxu) + (Lxu + Ŝxu)*res.M[k] + rs.K[k]'*(Luλ + Ŝuλ) + res.K[k]'*(Luu + Ŝuu)*res.M[k]
+        Q̂xx = Lxx + Ŝxx
+        Q̂xλ = Lxλ + Ŝxλ
+        Q̂ux = Lux_reg + Ŝxu'
+        Q̂uu = Luu_reg + Ŝuu
+        Q̂λu = Lλu + Ŝλu
+        Q̂λλ = Lλλ + Ŝλλ
+        Q̂x = Lx + Ŝx
+        Q̂u = Lu + Ŝu
+        Q̂λ = Lλ + Ŝλ
 
-        Sλx = Sxλ'
-        Sλλ = (Lλλ + Ŝλλ) + res.M[k]'*(Luλ + Ŝuλ) + res.M[k]'*(Luu + Ŝuu)*res.M[k]
+        Sxx = Q̂xx + Q̂ux'*res.K[k] + res.K[k]'*Q̂ux + res.K[k]'*Q̂uu*res.K[k]
+        Sxλ = Q̂xλ + Q̂ux'*res.M[k] + res.K[k]'*Q̂λu' + res.K[k]'*Q̂uu*res.M[k]
+        Sλλ = Q̂λλ + res.M[k]'*Q̂λu' + res.M[k]'*Q̂uu*res.M[k]
 
-        Sx = (Lx + Ŝx) + res.d[k]'*(Lux + Ŝux) + res.d[k]*(Luu + Ŝuu)*res.K[k] + (Lu + Ŝu)'*res.K[k]
-        Sλ = (Lλ + Ŝλ) + res.d[k]'*(Luλ + Ŝuλ) + res.d[k]*(Luu + Ŝuu)*res.M[k] + (Lu + Ŝu)'*res.M[k]
+        Sx = Q̂x + Q̂ux'*res.d[k] + res.K[k]'*Q̂uu*res.d[k] + res.K[k]'*Q̂u
+        Sλ = Q̂λ + Q̂λu*res.d[k] + res.M[k]'*Q̂uu*res.d[k] + res.M[k]'*Q̂u
 
         S[k][1:n,1:n] = Sxx
-        S[k][1:n,n+1:n+pN] = Sxλ
-        S[k][n+1:n+pN,1:n] = Sλx
-        S[k][n+1:n+pN,n+1:n+pN] = Sλλ
+        S[k][1:n,n+1:n+p] = Sxλ
+        S[k][n+1:n+p,1:n] = Sxλ'
+        S[k][n+1:n+p,n+1:n+p] = Sλλ
 
         s[k][1:n] = Sx
-        s[k][n+1:n+pN] = Sλ
+        s[k][n+1:n+p] = Sλ
 
-        Δv[1] += res.d[k]'*(Lu + Ŝu)
-        Δv[2] += 0.5*res.d[k]'*(Luu + Ŝuu)*res.d[k]
+        Δv[1] += res.d[k]'*Q̂u
+        Δv[2] += 0.5*res.d[k]'*Q̂uu*res.d[k]
 
-        # # Compute gains
-        # K[k] = -Quu_reg\Qux_reg
-        # d[k] = -Quu_reg\Qu
-        #
-        # # Calculate cost-to-go (using unregularized Quu and Qux)
-        # s[k] = Qx + K[k]'*Quu*d[k] + K[k]'*Qu + Qux'*d[k]
-        # S[k] = Qxx + K[k]'*Quu*K[k] + K[k]'*Qux + Qux'*K[k]
-        # S[k] = 0.5*(S[k] + S[k]')
-        #
-        # # calculated change is cost-to-go over entire trajectory
-        # Δv[1] += d[k]'*Qu
-        # Δv[2] += 0.5*d[k]'*Quu*d[k]
+        if k == 1
+            Δv[1] += res.dλ[1]'*Sλ
+            Δv[2] += 0.5*res.dλ[1]'*Sλλ*res.dλ[1]
+        end
 
         k = k - 1;
     end
@@ -271,3 +296,5 @@ function _backwardpass_active_set!(res::SolverVectorResults,solver::Solver)
 
     return Δv
 end
+
+a = 1
