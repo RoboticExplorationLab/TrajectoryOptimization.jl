5762f691851e397435e20225c4caee1c17a4bc96
Brian Jackson <bjack205@gmail.com>
2019-01-07 08:29:02 -0800

    Compare old and new objectives in solve_steps.jl

--- dev_notebooks/solve_steps.jl
+++ dev_notebooks/solve_steps.jl
@@ -15,13 +15,16 @@ Q = (1e-3)*Diagonal(I,n)
 R = (1e-2)*Diagonal(I,m)
 
 obj = UnconstrainedObjective(Q, R, Qf, tf, x0, xf)
+obj_new = LQRObjective(Q, R, Qf, tf, x0, xf)
 
 x_min = [-0.25; -0.001; -Inf]
 x_max = [0.25; 1.001; Inf]
 
 obj_con = TrajectoryOptimization.ConstrainedObjective(obj,x_min=x_min,x_max=x_max)
+obj_con_new = ConstrainedObjective(obj_new,x_min=x_min,x_max=x_max)
 
-solver = Solver(model,obj_con,N=N)
+solver = Solver(model,obj,N=N)
+solver_new = Solver(model,obj_new,N=N)
 solver.opts.verbose = true
 solver.opts.infeasible
 n,m = get_sizes(solver)
@@ -35,6 +38,9 @@ U0 = ones(m,N)
 #       INITIALIZATION       #
 #****************************#
 results = init_results(solver, X0, U0)
+results_new = init_results(solver_new, X0, U0)
+results.ρ[1] = 0
+results_new.ρ[1] = 0
 X,U = results.X, results.U
 X_,U_ = results.X_, results.U_
 
@@ -45,6 +51,7 @@ X_,U_ = results.X_, results.U_
 if !solver.opts.infeasible
     X[1] = solver.obj.x0
     flag = rollout!(results,solver) # rollout new state trajectoy
+    rollout!(results_new,solver_new)
 
     if !flag
         @info "Bad initial control sequence, setting initial control to zero"
@@ -59,15 +66,23 @@ if solver.opts.infeasible
         calculate_midpoints!(results, solver, results.X, results.U)
     end
     update_constraints!(results,solver,results.X,results.U)
+    update_constraints!(results_new,solver_new,results.X,results.U)
 end
-p = plot()
-plot_trajectory!(results)
+# p = plot()
+# plot_trajectory!(results)
 # plot(to_array(results.X)')
 
 J_prev = cost(solver, results)
+cost(solver_new, results_new)
+stage_cost(obj_new,X[5],U[5]) + 0.5*xf'Q*xf
+ℓ(X[5],U[5],Q,R,xf)
 TrajectoryOptimization.calculate_jacobians!(results, solver)
+calculate_jacobians!(results_new, solver_new)
 Δv = backwardpass!(results, solver)
+
+
 J = forwardpass!(results, solver, Δv)
+forwardpass!(results_new, solver_new, Δv)
 
 Profile.init(delay=1e-4)
 Profile.clear()
