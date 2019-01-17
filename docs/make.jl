1d7823448bd47157981101c79166ef78e6c34fec
Brian Jackson <bjack205@gmail.com>
2019-01-16 17:53:12 -0800

    Add Documenter stuff

--- docs/make.jl
+++ docs/make.jl
@@ -10,6 +10,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "https://github.com/RoboticExplorationLab/TrajectoryOptimization.jl.git"
+)
