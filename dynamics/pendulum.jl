da5f89e0d8c8af06790b02ef0d150c61baf24dff
Taylor Howell <thowell@stanford.edu>
2019-01-19 15:34:52 -0800

    simple pendulum tests w/ tight tolerances

--- dynamics/pendulum.jl
+++ dynamics/pendulum.jl
@@ -1,19 +1,5 @@
 ## Pendulum
 # https://github.com/HarvardAgileRoboticsLab/unscented-dynamic-programming/blob/master/pendulum_dynamics.m
-
-function pendulum_dynamics(x,u)
-    m = 1.
-    l = 0.5
-    b = 0.1
-    lc = 0.5
-    I = 0.25
-    g = 9.81
-    xdot = similar(x)
-    xdot[1] = x[2]
-    xdot[2] = (u[1] - m*g*lc*sin(x[1]) - b*x[2])/I
-    return xdot
-end
-
 function pendulum_dynamics!(xdot,x,u)
     m = 1.
     l = 0.5
@@ -25,8 +11,7 @@ function pendulum_dynamics!(xdot,x,u)
     xdot[2] = (u[1] - m*g*lc*sin(x[1]) - b*x[2])/I
 end
 n,m = 2,1
-model = Model(pendulum_dynamics,n,m)
-model! = Model(pendulum_dynamics!,n,m) # inplace model
+model = Model(pendulum_dynamics!,n,m) # inplace model
 
 # initial conditions
 x0 = [0; 0.]
@@ -45,15 +30,11 @@ tf = 5.
 obj_uncon = LQRObjective(Q, R, Qf, tf, x0, xf)
 
 # Constraints
-u_bound = 3
+u_bound = 2
 u_min = [-u_bound]
 u_max = [u_bound]
-x_min = [-5;-5]
-x_max = [10; 10]
-obj_con = ConstrainedObjective(obj_uncon, u_min=u_min, u_max=u_max, x_min=x_min, x_max=x_max) # constrained objective
+obj_con = ConstrainedObjective(obj_uncon, u_min=u_min, u_max=u_max) # constrained objective
 
 # Set up problem
 pendulum = [model,obj_uncon]
-pendulum! = [model!,obj_uncon]
 pendulum_constrained = [model, obj_con]
-pendulum_constrained! = [model!,obj_con]
