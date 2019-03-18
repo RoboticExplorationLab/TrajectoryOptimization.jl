d30904d0f8a0e9b6822a094b499d1b2e9e52bbc4
Brian Jackson <bjack205@gmail.com>
2019-03-18 09:22:10 -0700

    Build docs on the restructure branch

--- docs/make.jl
+++ docs/make.jl
@@ -6,12 +6,13 @@ makedocs(
     sitename = "TrajectoryOptimization",
     format = :html,
     modules = [TrajectoryOptimization],
-    # devbranch = "restructure",
+    devbranch = "restructure",
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/RoboticExplorationLab/TrajectoryOptimization.jl.git"
+    repo = "github.com/RoboticExplorationLab/TrajectoryOptimization.jl.git",
+    devbranch = "restructure",
 )
