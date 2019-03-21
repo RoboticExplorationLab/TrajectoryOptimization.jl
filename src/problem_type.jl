236f93d5ab2564e03058491509e9155f4c84609c
Brian Jackson <bjack205@gmail.com>
2019-03-21 16:14:24 -0700

    Changed problem constructors

--- src/problem_type.jl
+++ src/problem_type.jl
@@ -1,6 +1,6 @@
 "$(TYPEDEF) Trajectory Optimization Problem"
 struct Problem{T<:AbstractFloat}
-    model::Model
+    model::Model{Discrete}
     cost::CostFunction
     constraints::ConstraintSet
     x0::Vector{T}
@@ -8,34 +8,69 @@ struct Problem{T<:AbstractFloat}
     U::VectorTrajectory{T}
     N::Int
     dt::T
+
+    function Problem(model::Model, cost::CostFunction, constraints::ConstraintSet,
+        x0::Vector{T}, X::VectorTrajectory, U::VectorTrajectory, N::Int, dt::T) where T
+
+        n,m = model.n, model.m
+        @assert length(X[1]) == n
+        @assert length(U[1]) == m
+        @assert length(x0) == n
+        @assert length(X) == N
+
+        if length(U) == N
+            @info "Length of U should be N-1, not N. Trimming last entry"
+            U = U[1:end-1]
+        end
+        @assert length(U) == N-1
+        if !(dt > 0)
+            throw(ArgumentError("dt must be strictly positive"))
+        end
+
+        new{T}(model,cost,constraints,x0,X,U,N,dt)
+    end
 end
 
-Problem(model::Model,cost::CostFunction) = Problem{Float64}(model,cost,
-    AbstractConstraint[],[],Vector[],Vector[],0,0.0)
 
-Problem(T::Type,model::Model,cost::CostFunction) = Problem{T}(model,cost,
-    AbstractConstraint[],[],Vector[],Vector[],0,0.0)
 
-Problem(model::Model,cost::CostFunction,x0::Vector{T},U::VectorTrajectory{T}) where T = Problem{T}(model,
-    cost,AbstractConstraint[],x0,Vector[],U,length(U)+1,0.0)
+# Problem(model::Model,cost::CostFunction) = Problem{Float64}(model,cost,
+#     AbstractConstraint[],[],Vector[],Vector[],0,0.0)
 
-Problem(model::Model,cost::CostFunction,x0::Vector{T},U::VectorTrajectory{T},N::Int,dt::T) where T= Problem{T}(model,
-    cost,AbstractConstraint[],x0,Vector[],U,N,dt)
+# Problem(T::Type,model::Model,cost::CostFunction) = Problem{T}(model,cost,
+#     AbstractConstraint[],[],Vector[],Vector[],0,0.0)
 
-Problem(model::Model,cost::CostFunction,x0::Vector{T},X::VectorTrajectory{T},U::VectorTrajectory{T}) where T = Problem{T}(
-    model,cost,AbstractConstraint[],x0,X,U,length(X),0.0)
 
-Problem(model::Model,cost::CostFunction,x0::Vector{T},U::Matrix{T}) where T = Problem{T}(model,cost,
-    AbstractConstraint[],x0,Vector[],[U[:,k] for k = 1:size(U,2)],size(U,2)+1,0.0)
+function Problem(model::Model,cost::CostFunction,N::Int,dt::T) where T
+    X = empty_state(model.n,N)
+    U = [zeros(model.m) for k = 1:N-1]
+    Problem(model,cost,AbstractConstraint[],x0,X,U,N,dt)
+end
 
-Problem(model::Model,cost::CostFunction,x0::Vector{T},U::Matrix{T},N::Int,dt::T) where T = Problem{T}(model,cost,
-    AbstractConstraint[],x0,Vector[],[U[:,k] for k = 1:size(U,2)],N,dt)
+function Problem(model::Model,cost::CostFunction,x0::Vector{T},U::VectorTrajectory{T},dt::T) where T
+    N = length(U) + 1
+    X = empty_state(model.n,N)
+    Problem(model,cost,AbstractConstraint[],x0,X,U,N,dt)
+end
 
-Problem(model::Model,cost::CostFunction,x0::Vector{T},X::Matrix{T},U::Matrix{T}) where T = Problem{T}(
-    model,cost,AbstractConstraint[],x0,[X[:,k] for k = 1:size(X,2)],
-    [U[:,k] for k = 1:size(U,2)],size(X,2),0.0)
+# Problem(model::Model,cost::CostFunction,x0::Vector{T},U::VectorTrajectory{T},N::Int,dt::T) where T= Problem{T}(model,
+#     cost,AbstractConstraint[],x0,Vector[],U,N,dt)
+#
+# Problem(model::Model,cost::CostFunction,x0::Vector{T},X::VectorTrajectory{T},U::VectorTrajectory{T}) where T = Problem{T}(
+#     model,cost,AbstractConstraint[],x0,X,U,length(X),0.0)
+#
+# Problem(model::Model,cost::CostFunction,x0::Vector{T},U::Matrix{T}) where T = Problem{T}(model,cost,
+#     AbstractConstraint[],x0,Vector[],[U[:,k] for k = 1:size(U,2)],size(U,2)+1,0.0)
+#
+# Problem(model::Model,cost::CostFunction,x0::Vector{T},U::Matrix{T},N::Int,dt::T) where T = Problem{T}(model,cost,
+#     AbstractConstraint[],x0,Vector[],[U[:,k] for k = 1:size(U,2)],N,dt)
+#
+# Problem(model::Model,cost::CostFunction,x0::Vector{T},X::Matrix{T},U::Matrix{T}) where T = Problem{T}(
+#     model,cost,AbstractConstraint[],x0,[X[:,k] for k = 1:size(X,2)],
+#     [U[:,k] for k = 1:size(U,2)],size(X,2),0.0)
 
 Base.size(p::Problem) = (p.model.n,p.model.m,p.N)
+
+empty_state(n::Int,N::Int) = [ones(n)*NaN32 for k = 1:N]
 
 function update_problem(p::Problem;
     model=p.model,cost=p.cost,constraints=p.constraints,x0=p.x0,X=p.X,U=p.U,
@@ -92,6 +127,6 @@ function check_problem(p::Problem)
 end
 
 
-jacobian!(prob::Problem{T},solver) where T = jacobian!(solver.∇F,prob.model,prob.X,prob.U)
+jacobian!(prob::Problem{T},solver) where T = jacobian!(solver.∇F,prob.model,prob.X,prob.U,prob.dt)
 
 cost(prob::Problem,X=prob.X,U=prob.U) = cost(prob.cost,X,U,prob.dt)
