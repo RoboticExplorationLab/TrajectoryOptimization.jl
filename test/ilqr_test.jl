f7ea3b6268df17e71e4988bc9be44477603d9139
Brian Jackson <bjack205@gmail.com>
2019-03-21 16:15:47 -0700

    Update iLQR test

--- test/ilqr_test.jl
+++ test/ilqr_test.jl
@@ -3,14 +3,25 @@ N = 21
 solver = Solver(model,obj,N=N)
 n,m = get_sizes(solver)
 U0 = ones(m,N-1)
+X0 = rollout(solver,U0)
+cost(solver,X0,U0)
 res,stats = solve(solver,U0)
 
 costfun = obj.cost
 model_d = Model{Discrete}(model,rk4)
-prob = Problem(model_d,costfun)
-copyto!(prob.U,U0)
+U = to_dvecs(U0)
+X = empty_state(n,N)
+x0 = obj.x0
+dt = solver.dt
+C = AbstractConstraint[]
+prob = Problem(model_d,costfun,x0,U,dt)
+
 ilqr = iLQRSolver(prob)
-step!(prob,ilqr)
+rollout!(prob)
+J_prev = cost(prob)
+step!(prob,ilqr,J_prev)
 prob.U isa VectorTrajectory
 ilqr.∇F isa PartedMatTrajectory
 jacobian!(ilqr.∇F,prob.model,prob.X,prob.U,prob.dt)
+
+all(isfinite.(prob.X[1]))
