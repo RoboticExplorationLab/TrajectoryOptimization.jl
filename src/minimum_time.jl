84e3d97084fe9b90025dd3bfe3abfadbca8abe52
Brian Jackson <bjack205@gmail.com>
2019-05-14 19:36:57 -0700

    Update minimum time constraints to work off of ProblemConstraitns

--- src/minimum_time.jl
+++ src/minimum_time.jl
@@ -21,22 +21,22 @@ function minimum_time_problem(prob::Problem{T},R_min_time::T=1.0,dt_max::T=1.0,d
     push!(obj_mt,cost_mt)
 
     model_min_time = add_min_time_controls(prob.model)
-    con_min_time_eq, con_min_time_bnd = min_time_constraints(n,m,dt_max,dt_min)
+    constraints = mintime_constraints(prob,dt_max,dt_min)
 
-    con_prob = ConstraintSet[]
-    constrained = is_constrained(prob)
-    for k = 1:N-1
-        con_mt = AbstractConstraint[]
-        constrained ? append!(con_mt,update_constraint_set_jacobians(prob.constraints[k],prob.model.n,prob.model.n+1,prob.model.m)) : nothing
-        push!(con_mt,con_min_time_bnd)
-        k != 1 ? push!(con_mt,con_min_time_eq) : nothing
-        push!(con_prob,con_mt)
-    end
-    constrained ? push!(con_prob,prob.constraints[N]) : push!(con_prob,Constraint[])
+    # con_prob = ConstraintSet[]
+    # constrained = is_constrained(prob)
+    # for k = 1:N-1
+    #     con_mt = AbstractConstraint[]
+    #     constrained ? append!(con_mt,update_constraint_set_jacobians(prob.constraints[k],prob.model.n,prob.model.n+1,prob.model.m)) : nothing
+    #     push!(con_mt,con_min_time_bnd)
+    #     k != 1 ? push!(con_mt,con_min_time_eq) : nothing
+    #     push!(con_prob,con_mt)
+    # end
+    # constrained ? push!(con_prob,prob.constraints[N]) : push!(con_prob,Constraint[])
 
     # return con_prob, obj_mt
     update_problem(prob,model=model_min_time,obj=Objective(obj_mt),
-        constraints=ProblemConstraints(con_prob),
+        constraints=constraints,
         U=[[prob.U[k];sqrt(prob.dt)] for k = 1:prob.N-1],
         X=[[prob.X[k];sqrt(prob.dt)] for k = 1:prob.N],
         x0=[prob.x0;0.0])
@@ -54,7 +54,7 @@ function total_time(prob::Problem{T}) where T
     return tt
 end
 
-function min_time_equality(n::Int,m::Int)
+function mintime_equality(n::Int,m::Int)
     n̄ = n+1; m̄ = m+1; idx_h = n+m+2
     ∇con_eq = zeros(1,idx_h)
     ∇con_eq[1,idx_h] = 1.0
@@ -68,12 +68,25 @@ function min_time_equality(n::Int,m::Int)
     Constraint{Equality}(con_eq, jac_eq, 1, :min_time_eq, [collect(1:n̄), collect(1:m̄)], :stage)
 end
 
-function mintime_bounds(prob::Problem, dt_max::T=1.0, dt_min::T=1e-3) where T
+function mintime_constraints(prob::Problem, dt_max::T=1.0, dt_min::T=1e-3) where T
+    n,m,N = size(prob)
+    mt_eq = mintime_equality(n,m)
     PC = copy(prob.constraints)
     mt_bnd = BoundConstraint(1,1, u_max=sqrt(dt_max), u_min=sqrt(dt_min))
-    for C in PC
-        bnd = remove_bounds!(C)
-        C += [bnd; mt_bnd]
+    bnd0 = BoundConstraint(n,m)
+    for k = 1:N
+        bnd = remove_bounds!(PC[k])
+        if isempty(bnd)
+            bnd = bnd0
+        else
+            bnd = bnd[1]
+        end
+        bnd2 = combine(bnd,mt_bnd)
+        PC[k] = update_constraint_set_jacobians(PC[k], n, n+1, m)
+        PC[k] += bnd2
+        if 1 < k < N
+            PC[k] += mt_eq
+        end
     end
     return PC
 end
