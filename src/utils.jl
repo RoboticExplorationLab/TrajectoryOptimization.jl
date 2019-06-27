1c122599a3f51ce2aef2799b1b3fef3d95a57160
taylor <thowell@stanford.edu>
2019-06-27 16:10:46 -0700

    update pro. dep., test fixes, added interp_rows

--- src/utils.jl
+++ src/utils.jl
@@ -2,6 +2,18 @@ import Base: convert, copyto!, Array, vec, println, copy
 import LinearAlgebra: norm
 import Plots: plot, plot!
 
+function interp_rows(N::Int,tf::Float64,X::AbstractMatrix)::Matrix
+    n,N1 = size(X)
+    t1 = range(0,stop=tf,length=N1)
+    t2 = collect(range(0,stop=tf,length=N))
+    X2 = zeros(n,N)
+    for i = 1:n
+        interp_cubic = CubicSplineInterpolation(t1, X[i,:])
+        X2[i,:] = interp_cubic(t2)
+    end
+    return X2
+end
+
 pos(x) = max(0,x)
 
 function to_array(X::Vector{<:AbstractArray})
