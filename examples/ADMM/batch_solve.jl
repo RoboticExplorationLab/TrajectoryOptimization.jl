da6531fb14149bab38a09948fb09d703d4b30595
Brian Jackson <bjack205@gmail.com>
2019-08-27 17:52:36 -0700

    Add collision constraint to batch

--- examples/ADMM/batch_solve.jl
+++ examples/ADMM/batch_solve.jl
@@ -1,3 +1,59 @@
+using Combinatorics
+
+function visualize_batch(vis, sol)
+    x_inds = [(i-1)*n_lift .+ (1:n_lift) for i = 1:num_lift]
+    push!(x_inds, num_lift*n_lift .+ (1:n_load))
+
+    # Params
+    r_lift = 0.1
+
+    X = sol.X
+    is_quad = n_lift == 13
+
+    # Quad mesh
+    if is_quad
+        traj_folder = joinpath(dirname(pathof(TrajectoryOptimization)),"..")
+        urdf_folder = joinpath(traj_folder, "dynamics","urdf")
+        obj = joinpath(urdf_folder, "quadrotor_base.obj")
+
+        quad_scaling = 0.085
+        robot_obj = FileIO.load(obj)
+        robot_obj.vertices .= robot_obj.vertices .* quad_scaling
+        robot_mat = MeshPhongMaterial(color=RGBA(0, 0, 0, 1.0))
+    else
+        robot_obj = HyperSphere(Point3f0(0), convert(Float32,r_lift))
+        robot_mat = MeshPhongMaterial(color=RGBA(0, 0, 0, 1.0))
+    end
+
+    cable = Cylinder(Point3f0(0,0,0),Point3f0(0,0,d),convert(Float32,0.01))
+    for i = 1:num_lift
+        setobject!(vis["lift"]["$i"], robot_obj, robot_mat)
+        setobject!(vis["cable"]["$i"], cable, MeshPhongMaterial(color=RGBA(1,0,0,1)))
+    end
+    setobject!(vis["load"], HyperSphere(Point3f0(0), Float32(0.1)), MeshPhongMaterial(color=RGBA(0,0,1,1)))
+
+    anim = MeshCat.Animation(convert(Int,floor(1.0/sol.dt)))
+    for k = 1:sol.N
+        MeshCat.atframe(anim,vis,k) do frame
+            x_load = X[k][x_inds[end]]
+            settransform!(frame["load"], Translation(x_load[1:3]))
+
+            for i = 1:num_lift
+                x_lift = X[k][x_inds[i]]
+                settransform!(frame["cable"]["$i"], cable_transform(x_lift[1:3], x_load[1:3]))
+                if is_quad
+                    settransform!(frame["lift"]["$i"], compose(Translation(x_lift[1:3]), LinearMap(Quat(x_lift[4:7]...))))
+                else
+                    settransform!(frame["lift"]["$i"], Translation(x_lift[1:3]))
+                end
+
+            end
+        end
+    end
+    MeshCat.setanimation!(vis,anim)
+    return anim
+end
+
 
 function gen_batch_dynamics(lift_model, load_model, num_lift)
     n_lift, m_lift = lift_model.n, lift_model.m
@@ -51,6 +107,8 @@ N = 51
 tf = 5.0
 d = 1.5
 α = pi/4
+r_lift = 0.2
+r_load = 0.1
 
 # Initial and final conditions
 r0_load = [0.0, 0, 0]
@@ -129,10 +187,25 @@ function force_sign(c,x,u)
 end
 con_sign = Constraint{Inequality}(force_sign, n, m, num_lift, :sign)
 
+function self_collision(c,x,u)
+    x_inds = [(i-1)*n_lift .+ (1:n_lift) for i = 1:num_lift]
+    push!(x_inds, num_lift*n_lift .+ (1:n_load))
+
+    pairs = combinations(collect(1:num_lift), 2)
+    for (k,pair) in enumerate(pairs)
+        i,j = pair
+        x_i = x[x_inds[i]][1:2]
+        x_j = x[x_inds[j]][1:2]
+        dist = x_j - x_i
+        c[k] = 2r_lift - norm(dist)
+    end
+end
+con_collision = Constraint{Inequality}(self_collision, n, m, binomial(num_lift, 2), :collision)
+
 
 constraints = Constraints(N)
 for k = 1:N-1
-    constraints[k] += con_direction + con_sign
+    constraints[k] += con_direction + con_sign + con_collision
 end
 
 # Initial controls
@@ -153,60 +226,8 @@ evaluate!(xdot, batch_model, x0, u0)
 # Solve
 prob = Problem(rk3(batch_model), obj, U0, x0=x0, xf=xf, constraints=constraints, N=N, tf=tf)
 res, = solve(prob, opts_al)
+findmax_violation(res)
 visualize_batch(vis,res)
 
 
-function visualize_batch(vis, sol)
-    x_inds = [(i-1)*n_lift .+ (1:n_lift) for i = 1:num_lift]
-    push!(x_inds, num_lift*n_lift .+ (1:n_load))
-
-    # Params
-    r_lift = 0.1
-
-    X = sol.X
-    is_quad = n_lift == 13
-
-    # Quad mesh
-    if is_quad
-        traj_folder = joinpath(dirname(pathof(TrajectoryOptimization)),"..")
-        urdf_folder = joinpath(traj_folder, "dynamics","urdf")
-        obj = joinpath(urdf_folder, "quadrotor_base.obj")
-
-        quad_scaling = 0.085
-        robot_obj = FileIO.load(obj)
-        robot_obj.vertices .= robot_obj.vertices .* quad_scaling
-        robot_mat = MeshPhongMaterial(color=RGBA(0, 0, 0, 1.0))
-    else
-        robot_obj = HyperSphere(Point3f0(0), convert(Float32,r_lift))
-        robot_mat = MeshPhongMaterial(color=RGBA(0, 0, 0, 1.0))
-    end
-
-    cable = Cylinder(Point3f0(0,0,0),Point3f0(0,0,d),convert(Float32,0.01))
-    for i = 1:num_lift
-        setobject!(vis["lift"]["$i"], robot_obj, robot_mat)
-        setobject!(vis["cable"]["$i"], cable, MeshPhongMaterial(color=RGBA(1,0,0,1)))
-    end
-    setobject!(vis["load"], HyperSphere(Point3f0(0), Float32(0.1)), MeshPhongMaterial(color=RGBA(0,0,1,1)))
-
-    anim = MeshCat.Animation(convert(Int,floor(1.0/sol.dt)))
-    for k = 1:sol.N
-        MeshCat.atframe(anim,vis,k) do frame
-            x_load = X[k][x_inds[end]]
-            settransform!(frame["load"], Translation(x_load[1:3]))
-
-            for i = 1:num_lift
-                x_lift = X[k][x_inds[i]]
-                settransform!(frame["cable"]["$i"], cable_transform(x_lift[1:3], x_load[1:3]))
-                if is_quad
-                    settransform!(frame["lift"]["$i"], compose(Translation(x_lift[1:3]), LinearMap(Quat(x_lift[4:7]...))))
-                else
-                    settransform!(frame["lift"]["$i"], Translation(x_lift[1:3]))
-                end
-
-            end
-        end
-    end
-    MeshCat.setanimation!(vis,anim)
-    return anim
-end
 visualize_batch(vis,res)
