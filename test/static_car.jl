c97f4ac8d186237b18e06f9b1e393dc7dfb81205
Brian Jackson <bjack205@gmail.com>
2019-12-12 19:59:32 -0800

    Static car escape working now

--- test/static_car.jl
+++ test/static_car.jl
@@ -1,11 +1,172 @@
-car = Dynamics.DubinsCar()
-n,m = size(car)
-x = @SVector rand(n); u = @SVector rand(m);
-z = KnotPoint(x,u,0.1)
+prob = copy(Problems.car_3obs_static)
+Z0 = copy(prob.Z)
+X0 = deepcopy(states(Z0))
+U0 = deepcopy(controls(Z0))
 
-@btime dynamics($car, $z)
+# Solve with iLQR
+ilqr = StaticiLQRSolver(prob)
+solve!(ilqr)
+@test cost(ilqr) < 0.8182
+initial_controls!(ilqr, U0)
+@test (@allocated solve!(ilqr)) == 0
 
-discrete_dynamics(RK3, car, x, u, 0.1)
-@btime discrete_dynamics(RK3, $car,$x,$u, 0.1)
-@btime discrete_dynamics(RK3, $car, $z)
-@btime discrete_jacobian(RK3, $car, $z)
+# Solve with AL-iLQR
+al = StaticALSolver(prob)
+initial_controls!(al, U0)
+solve!(al)
+@test max_violation(al) < al.opts.constraint_tolerance
+@test cost(al) < 0.82
+initial_controls!(al, U0)
+@test (@allocated solve!(al)) == 0
+
+# Solve with ALTRO
+altro = StaticALTROSolver(prob)
+altro.opts.opts_pn.verbose = false
+initial_controls!(altro, U0)
+solve!(altro)
+Z_sol = copy(get_trajectory(altro))
+X_sol = deepcopy(states(altro))
+@test max_violation(altro) < 1e-8
+@test cost(altro) < 0.82
+altro.opts.projected_newton = false
+initial_controls!(altro, U0)
+solve!(altro)
+initial_controls!(altro, U0)
+@test (@allocated solve!(altro)) == 0
+
+
+
+# Escape problem
+T = Float64
+max_con_viol = 1e-3
+opts_ilqr = iLQRSolverOptions()
+sopts_ilqr = StaticiLQRSolverOptions()
+
+opts_al = AugmentedLagrangianSolverOptions(
+    opts_uncon=opts_ilqr,
+    cost_tolerance=1.0e-6,
+    cost_tolerance_intermediate=1.0e-2,
+    constraint_tolerance=max_con_viol,
+    penalty_scaling=50.,
+    penalty_initial=10.)
+
+sopts_al = StaticALSolverOptions(
+    opts_uncon=sopts_ilqr,
+    cost_tolerance=1.0e-6,
+    cost_tolerance_intermediate=1.0e-2,
+    constraint_tolerance=max_con_viol,
+    penalty_scaling=50.,
+    penalty_initial=10.)
+
+opts_altro = ALTROSolverOptions{T}(
+    opts_al=opts_al,
+    R_inf=1.0e-1,
+    resolve_feasible_problem=false,
+    opts_pn=opts_pn,
+    projected_newton=false,
+    projected_newton_tolerance=1.0e-3);
+
+sopts_altro = StaticALTROSolverOptions(
+    opts_al=sopts_al,
+    R_inf=1.0e-1,
+    resolve_feasible_problem=false,
+    opts_pn=sopts_pn,
+    projected_newton=false,
+    projected_newton_tolerance=1.0e-3);
+
+prob = copy(Problems.car_escape)
+sprob = copy(Problems.car_escape_static)
+states(sprob) == prob.X
+controls(sprob) == prob.U
+
+prob = infeasible_problem(prob, opts_altro.R_inf)
+sprob = InfeasibleProblem(sprob, sprob.Z, opts_altro.R_inf/prob.dt)
+
+al = AugmentedLagrangianSolver(prob)
+sal = StaticALSolver(sprob)
+
+solve!(prob, al)
+solve!(sal)
+
+max_violation(prob)
+max_violation(sal)
+
+
+# Solve with ALTRO
+prob = copy(Problems.car_escape)
+sprob = copy(Problems.car_escape_static)
+states(sprob) == prob.X
+controls(sprob) == prob.U
+
+prob = copy(Problems.car_escape)
+solve!(prob, opts_altro)
+max_violation_direct(prob)
+
+sprob = copy(Problems.car_escape_static)
+altro = StaticALTROSolver(sprob, sopts_altro, infeasible=true)
+Z0 = copy(get_trajectory(altro))
+initial_trajectory!(altro, Z0)
+altro.opts.projected_newton = false
+solve!(altro)
+max_violation(altro)
+Z = get_trajectory(altro.solver_al)
+
+pn = altro.solver_pn
+initial_trajectory!(pn, Z)
+viol0 = max_violation(pn)
+reset!(pn)
+
+solver = pn
+update_constraints!(solver)
+copy_constraints!(solver)
+constraint_jacobian!(solver)
+copy_jacobians!(solver)
+cost_expansion!(solver)
+update_active_set!(solver)
+max_violation(pn)
+norm(active_constraints(pn)[2],Inf)
+max_violation(pn) == norm(active_constraints(pn)[2],Inf)
+conSet = get_constraints(pn)
+
+copyto!(solver.P, solver.Z)
+copy_constraints!(solver)
+copy_jacobians!(solver)
+copy_active_set!(solver)
+
+D,d = active_constraints(solver)
+norm(d,Inf) == viol0
+H = Diagonal(solver.H)
+HinvD = H\D'
+ρ = 1e-2
+S = Symmetric(D*HinvD)
+Sreg = cholesky(S + ρ*I)
+
+P = solver.P
+P̄ = copy(solver.P)
+
+# line search
+α = 1.0
+S = (S,Sreg)
+δλ = reg_solve(S[1], d, S[2], 1e-8, 25)
+δZ = -HinvD*δλ
+P̄.Z .= P.Z + α*δZ
+
+copyto!(solver.Z̄, P̄)
+update_constraints!(solver, solver.Z̄)
+update_constraints!(solver, Z)
+max_violation(solver)
+findmax(map(conSet.constraints) do con
+    maximum(con.c_max)
+end)
+findmax(maximum.(conSet[3].vals))
+findmax(conSet[3].vals[71])
+conSet[3].active[71][170]
+conSet[3].vals[71][170]
+update_active_set!(conSet, Z, Val(0.001))
+pn.opts.active_set_tolerance
+
+copy_constraints!(solver)
+update_active_set!(solver)
+copy_active_set!(solver)
+d = active_constraints(solver)[2]
+norm(d,Inf)
--- test/static_pn_tests.jl
+++ test/static_pn_tests.jl
@@ -1,61 +1,142 @@
-using StaticArrays, LinearAlgebra, BenchmarkTools, ForwardDiff, SparseArrays
+# Escape problem
+T = Float64
+max_con_viol = 1e-4
+opts_ilqr = iLQRSolverOptions()
+sopts_ilqr = StaticiLQRSolverOptions()
 
-n,m,N = 3,2,11
-dt = 0.1
-NN = n*N + m*(N-1)
-xs = @SVector rand(n)
-us = @SVector rand(m)
-Hs = @SMatrix rand(n,n)
-Xs = [xs for k = 1:N]
-Us = [us for k = 1:N-1]
-Zs = [KnotPoint(xs,us,dt) for k = 1:N]
-Zs[end] = KnotPoint(xs,m)
+opts_al = AugmentedLagrangianSolverOptions(
+    opts_uncon=opts_ilqr,
+    cost_tolerance=1.0e-6,
+    cost_tolerance_intermediate=1.0e-2,
+    constraint_tolerance=max_con_viol,
+    penalty_scaling=50.,
+    penalty_initial=10.)
 
-Z = zeros(NN)
-xinds = [SVector{n}((n+m)*(k-1) .+ (1:n)) for k = 1:N]
-uinds = [SVector{m}(n + (n+m)*(k-1) .+ (1:m)) for k = 1:N-1]
+sopts_al = StaticALSolverOptions(
+    opts_uncon=sopts_ilqr,
+    cost_tolerance=1.0e-6,
+    cost_tolerance_intermediate=1.0e-2,
+    constraint_tolerance=max_con_viol,
+    penalty_scaling=50.,
+    penalty_initial=10.)
 
-P = StaticPrimals(n,m,N)
-copyto!(P,Zs)
-@btime copyto!($P,$Zs)
-copyto!(Zs,P)
-@btime copyto!($Zs,$P)
+opts_altro = ALTROSolverOptions{T}(
+    opts_al=opts_al,
+    R_inf=1.0e-1,
+    resolve_feasible_problem=false,
+    opts_pn=opts_pn,
+    projected_newton=false,
+    projected_newton_tolerance=1.0e-3);
 
+sopts_altro = StaticALTROSolverOptions(
+    opts_al=sopts_al,
+    R_inf=1.0e-1,
+    resolve_feasible_problem=false,
+    opts_pn=sopts_pn,
+    projected_newton=false,
+    projected_newton_tolerance=1.0e-3);
 
-# Constraints
-xf = @SVector [5.,5,0]
+prob = copy(Problems.car_escape)
+sprob = copy(Problems.car_escape_static)
 
-x_min = @SVector [-10,-10,-Inf]
-x_max = @SVector [10,10,Inf]
-u_min = @SVector [-2.,0]
-u_max = @SVector [2.,3]
-bnd = StaticBoundConstraint(n,m, x_min=x_min, x_max=x_max,
-    u_min=u_min, u_max=u_max)
-bnd_con = ConstraintVals(bnd, 1:N-1)
-goal = GoalConstraint(xf)
-goal_con = ConstraintVals(goal, N:N)
-cons = [bnd_con, goal_con]
-conSet = ConstraintSets(cons, N)
+prob = infeasible_problem(prob, opts_altro.R_inf)
+sprob = InfeasibleProblem(sprob, sprob.Z, opts_altro.R_inf/prob.dt)
 
-sum(conSet.p) + n*N
-sum(length.(dyn))
-sum([sum(length.(con)) for con in cons])
-dyn,cons = gen_con_inds(n,m,conSet)
-cons isa Vector{Vector{T} where T}
+# Solve with augmented lagrangian
+al = AugmentedLagrangianSolver(prob, opts_al)
+sal = StaticALSolver(sprob, sopts_al)
 
-function con_labels(dyn,cons)
-    NP = sum(length.(dyn)) + sum([sum(length.(con)) for con in cons])
-    labels = Vector{String}(undef,NP)
-    for d in dyn
-        labels[d] .= "dynamics"
-    end
-    for i in eachindex(cons)
-        for c in cons[i]
-            labels[c] .= "constraint $i"
-        end
-    end
-    labels[dyn[1]] .= "initial condition"
-    return labels
+solve!(prob, al)
+solve!(sal)
 
+max_violation(prob)
+max_violation(sal)
+
+# Solve with ALTRO
+prob = copy(Problems.car_escape)
+sprob = copy(Problems.car_escape_static)
+Z0 = copy(sprob.Z)
+X0 = states(Z0)
+U0 = controls(Z0)
+
+opts_altro.projected_newton = true
+initial_controls!(prob, U0)
+initial_states!(prob, X0)
+solve!(prob, opts_altro)
+max_violation(prob)
+
+saltro = StaticALTROSolver(sprob, sopts_altro, infeasible=true)
+Z_init = copy(get_trajectory(saltro))
+saltro.opts.projected_newton = true
+initial_trajectory!(saltro, Z_init)
+solve!(saltro)
+max_violation(saltro)
+
+@btime begin
+    initial_controls!($prob, $U0)
+    initial_states!($prob, $X0)
+    solve!($prob, $opts_altro)
 end
-con_labels(dyn,cons)
+
+@btime begin
+    initial_trajectory!($saltro, $Z_init)
+    solve!($saltro)  # 87x faster!
+end
+
+
+
+# Create Solvers
+# pn = ProjectedNewtonSolver(prob)
+# spn = StaticPNSolver(sprob)
+pn = altro.solver_pn
+spn = saltro.solver_pn
+
+pn.V = PrimalDual(prob)
+copyto!(spn.P, spn.Z)
+pn.V.Z ≈ spn.P.Z
+
+# Update everything
+V = pn.V
+dynamics_constraints!(prob, pn, V)
+update_constraints!(prob, pn, V)
+update_constraints!(spn)
+copy_constraints!(spn)
+spn.d ≈ pn.y
+
+dynamics_jacobian!(prob, pn, V)
+constraint_jacobian!(prob, pn, V)
+constraint_jacobian!(spn)
+copy_jacobians!(spn)
+spn.D ≈ pn.Y
+
+active_set!(prob, pn)
+update_active_set!(spn)
+copy_active_set!(spn)
+spn.active_set == pn.a.duals
+
+Y,y = active_constraints(prob, pn)
+D,d = active_constraints(spn)
+Y ≈ D
+y ≈ d
+
+cost_expansion!(prob, pn, V)
+cost_expansion!(spn)
+spn.H == pn.H
+H = Diagonal(spn.H)
+
+# Compute Search Direction
+ρ = 1e-2
+
+HinvD = H\D'
+HinvY = H\Y'
+
+Sd = Symmetric(D*HinvD)
+Sy = Symmetric(Y*HinvY)
+Sd ≈ Sy
+
+Sd_reg = cholesky(Sd + ρ*I)
+Sy_reg = cholesky(Sy + ρ*I)
+
+norm(spn.d[spn.active_set],Inf)
+_projection_linesearch!(prob, pn, V, (Sy, Sy_reg), HinvY)
+_projection_linesearch!(spn, (Sd, Sd_reg), HinvD)
