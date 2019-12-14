51ecbd92cdbca973070fa139abc6acb36b64576c
Brian Jackson <bjack205@gmail.com>
2019-12-14 07:54:47 -0800

    Clean out problems

--- problems/doubleintegrator.jl
+++ problems/doubleintegrator.jl
@@ -1,35 +1,3 @@
-# # Double Integrator
-# T = Float64
-# model = TrajectoryOptimization.Dynamics.doubleintegrator
-# model_d = rk3(model)
-# n = model.n; m = model.m
-#
-# # costs
-# Q = 1.0*Diagonal(I,n)
-# Qf = 1.0*Diagonal(I,n)
-# R = 1.0e-1*Diagonal(I,m)
-# x0 = [0; 0.]
-# xf = [1.; 0] # (ie, swing up)
-#
-# N = 21
-# dt = 0.1
-# U0 = [0.001*rand(m) for k = 1:N-1]
-# obj = TrajectoryOptimization.LQRObjective(Q,R,Qf,xf,N)
-#
-# u_max = 1.5
-# u_min = -1.5
-#
-# bnd = BoundConstraint(n,m,u_max=u_max, u_min=u_min,trim=true)
-# goal = goal_constraint(xf)
-# constraints = Constraints(N)
-# for k = 1:N-1
-#     constraints[k] += bnd
-# end
-# constraints[N] += goal
-#
-# doubleintegrator = TrajectoryOptimization.Problem(model_d, obj, constraints=constraints, x0=x0, xf=xf, N=N, dt=dt)
-# initial_controls!(doubleintegrator, U0)
-#
 
 model = Dynamics.DoubleIntegrator()
 n,m = size(model)
@@ -55,4 +23,4 @@ bnd = StaticBoundConstraint(n,m, u_min=-u_bnd, u_max=u_bnd)
 con_bnd = ConstraintVals(bnd, 1:N-1)
 conSet = ConstraintSets(n,m,[con_bnd], N)
 
-doubleintegrator_static = StaticProblem(model, obj, xf, tf, constraints=conSet, x0=x0, N=N)
+doubleintegrator_static = Problem(model, obj, xf, tf, constraints=conSet, x0=x0, N=N)
--- problems/pendulum.jl
+++ problems/pendulum.jl
@@ -1,40 +1,3 @@
-# model = TrajectoryOptimization.Dynamics.pendulum
-# n = model.n; m = model.m
-# model_d = rk3(model)
-#
-# # cost
-# Q = Array(1e-3*Diagonal(I,n))
-# R = Array(1e-3*Diagonal(I,m))
-# Qf = Q
-# x0 = zeros(n)
-# xf = [pi;0.0]
-#
-# # options
-# verbose=false
-# opts_ilqr = iLQRSolverOptions{T}(verbose=false,live_plotting=:off)
-# opts_al = AugmentedLagrangianSolverOptions{T}(verbose=false,opts_uncon=opts_ilqr,iterations=50,penalty_scaling=10.0)
-# opts_altro = ALTROSolverOptions{T}(verbose=false,opts_al=opts_al,R_minimum_time=15.0,dt_max=0.15,dt_min=1.0e-3)
-#
-# # constraints
-# u_bnd = 3.
-# bnd = BoundConstraint(n,m,u_min=-u_bnd,u_max=u_bnd)
-# goal_con = goal_constraint(xf)
-#
-# # problem
-# N = 31
-# U = [ones(m) for k = 1:N-1]
-# constraints = Constraints(N)
-# for k = 1:N-1
-#     constraints[k] += bnd
-# end
-# constraints[N] += goal_con
-# obj = LQRObjective(Q,R,Qf,xf,N)
-#
-# N = 31
-# dt = 0.15
-# pendulum = Problem(model_d,obj,U,constraints=constraints,dt=dt,x0=x0,xf=xf,N=N)
-
-# Static Problem
 model = Dynamics.Pendulum()
 n,m = size(model)
 
@@ -57,5 +20,5 @@ conSet = ConstraintSets(n,m,[con_bnd, con_xf],N)
 
 # problem
 U = [@SVector ones(m) for k = 1:N-1]
-pendulum_static = StaticProblem(model, obj, xf, tf, constraints=conSet, x0=x0)
+pendulum_static = Problem(model, obj, xf, tf, constraints=conSet, x0=x0)
 initial_controls!(pendulum_static, U)
