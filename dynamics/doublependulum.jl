05886cc2ad7d1187b9460da82bcf8996b00d00ee
Brian Jackson <bjack205@gmail.com>
2020-06-08 16:01:06 -0700

    Remove dynamics

--- dynamics/doublependulum.jl
+++
@@ -1,7 +0,0 @@
-## Double Pendulum
-import TrajectoryOptimization
-traj_folder = joinpath(dirname(pathof(TrajectoryOptimization)),"..")
-urdf_folder = joinpath(traj_folder, "dynamics/urdf")
-urdf_doublependulum = joinpath(urdf_folder, "doublependulum.urdf")
-
-doublependulum = Model(urdf_doublependulum)
