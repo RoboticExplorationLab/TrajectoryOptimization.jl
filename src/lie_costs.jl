d7b9da59f3a1b9bc55fadbd0e7d0178b99b825aa
Brian Jackson <bjack205@gmail.com>
2020-10-07 15:23:21 -0400

    Add DiagLieCost

---
+++ src/lie_costs.jl
@@ -0,0 +1,169 @@
+for rot in (:UnitQuaternion, :MRP, :RodriguesParam)
+    @eval rotation_name(::Type{<:$rot}) = $rot
+end
+
+struct DiagonalLieCost{n,m,T,nV,nR,Rot} <: QuadraticCostFunction{n,m,T}
+    Q::SVector{nV,T}
+    R::Diagonal{T,SVector{m,T}}
+    q::SVector{nV,T}
+    r::SVector{m,T}
+    c::T
+    w::Vector{T}                     # weights on rotations (1 per rotation)
+    vinds::SVector{nV,Int}           # inds of vector states
+    qinds::Vector{SVector{nR,Int}}   # inds of rot states
+    qrefs::Vector{UnitQuaternion{T}} # reference rotations
+    function DiagonalLieCost(s::RD.LieState{Rot,P},
+            Q::AbstractVector{<:Real}, R::AbstractVector{<:Real}, 
+            q::AbstractVector{<:Real}, r::AbstractVector{<:Real},
+            c::Real, w::AbstractVector,
+            qrefs::Vector{<:Rotation}
+        ) where {Rot,P}
+        n = length(s)
+        m = length(R)
+        nV = sum(P)
+        nR = Rotations.params(Rot)
+        num_rots = length(P)-1
+        @assert length(Q) == length(q) == nV
+        @assert length(r) == m 
+        @assert length(qrefs) == length(w) == num_rots
+        vinds = [RobotDynamics.vec_inds(Rot, P, i) for i = 1:num_rots+1]
+        rinds = [RobotDynamics.rot_inds(Rot, P, i) for i = 1:num_rots]
+        vinds = SVector{nV}(vcat(vinds...))
+        rinds = SVector{nR}.(rinds)
+        qrefs = UnitQuaternion.(qrefs)
+        T = promote_type(eltype(Q), eltype(R), eltype(q), eltype(r), typeof(c), eltype(w))
+        R = Diagonal(SVector{m}(R))
+        new{n,m,T,nV,nR,rotation_name(Rot)}(Q, R, q, r, c, w, vinds, rinds, qrefs)
+    end
+end
+
+function DiagonalLieCost(s::RD.LieState{Rot,P}, 
+        Q::Vector{<:AbstractVector{Tq}}, 
+        R::Union{<:Diagonal{Tr},<:AbstractVector{Tr}};
+        q = [zeros(Tq,p) for p in P], 
+        r=zeros(Tr,size(R,1)), 
+        c=0.0, 
+        w=ones(RobotDynamics.num_rotations(s)),
+        qrefs=[one(UnitQuaternion) for i in 1:RobotDynamics.num_rotations(s)]
+    ) where {Rot,P,Tq,Tr}
+    @assert length.(Q) == collect(P) == length.(q)
+    Q = vcat(Q...)
+    q = vcat(q...)
+    if R isa Diagonal
+        R = R.diag
+    end
+    DiagonalLieCost(s, Q, R, q, r, c, w, qrefs)
+end
+
+function DiagonalLieCost(s::RD.LieState{Rot,P}, Q::Diagonal, R::Diagonal; 
+        q::AbstractVector=zero(Q.diag), kwargs...
+    ) where {Rot,P}
+    if length(Q.diag) == sum(P)
+        vinds = cumsum(insert(SVector(P), 1, 0))
+        vinds = [vinds[i]+1:vinds[i+1] for i = 1:length(P)]
+        Qs = [Q.diag[vind] for vind in vinds]
+        qs = [q[vind] for vind in vinds]
+        DiagonalLieCost(s, Qs, R.diag; q=qs, kwargs...)
+    else
+        num_rots = length(P) - 1
+        vinds = [RobotDynamics.vec_inds(Rot, P, i) for i = 1:num_rots+1]
+        rinds = [RobotDynamics.rot_inds(Rot, P, i) for i = 1:num_rots]
+
+        # Split Q and q into vector and rotation components
+        Qv = [Q[vind] for vind in vinds]
+        Qr = [Q[rind] for rind in rinds]
+        qv = [q[vind] for vind in vinds]
+
+        # Use sum of diagonal Q as w
+        w = sum.(Qr)
+        @show vinds
+
+        DiagonalLieCost(s, Qv, R.diag; q=qv, w=w, kwargs...)
+    end
+end
+
+RobotDynamics.rotation_type(::DiagonalLieCost{<:Any,<:Any,<:Any, <:Any,<:Any, Rot}) where Rot = Rot
+RobotDynamics.state_dim(::DiagonalLieCost{n}) where n = n
+RobotDynamics.control_dim(::DiagonalLieCost{<:Any,m}) where m = m
+is_blockdiag(::DiagonalLieCost) = true
+is_diag(::DiagonalLieCost) = true
+
+function stage_cost(cost::DiagonalLieCost, x::AbstractVector)
+    Rot = RobotDynamics.rotation_type(cost)
+    Jv = veccost(cost.Q, cost.q, x, cost.vinds)
+    Jr = quatcost(Rot, cost.w, x, cost.qinds, cost.qrefs)
+    return Jv + Jr
+end
+
+function veccost(Q, q, x, vinds)
+    xv = x[vinds]
+    0.5*xv'Diagonal(Q)*xv + q'xv
+end
+
+function quatcost(::Type{Rot}, w, x, qinds, qref) where Rot<:Rotation
+    J = zero(eltype(x))
+    for i = 1:length(qinds) 
+        # q = Rotations.params(UnitQuaternion(Rot(x[qinds[i]])))
+        q = toquat(Rot, x[qinds[i]])
+        qd = Rotations.params(qref[i])
+        err = q'qd
+        J = w[i]*min(1-err,1+err)
+    end
+    return J
+end
+
+function gradient!(E::QuadraticCostFunction, cost::DiagonalLieCost, x::AbstractVector)
+    # Vector states
+    Rot = RobotDynamics.rotation_type(cost)
+    xv = x[cost.vinds]
+    E.q[cost.vinds] .= cost.Q .* xv + cost.q
+
+    # Quaternion states
+    for i = 1:length(cost.qinds)
+        qind = cost.qinds[i]
+        q = toquat(Rot, x[qind])
+        qref = Rotations.params(cost.qrefs[i])
+        dq = q'qref
+        jac = Rotations.jacobian(UnitQuaternion, Rot(q))
+        E.q[qind] .= -cost.w[i]*sign(dq)*jac'qref
+    end
+    return false
+end
+
+function hessian!(E::QuadraticCostFunction, cost::DiagonalLieCost{n}, x::AbstractVector) where n
+    for (i,j) in enumerate(cost.vinds) 
+        E.Q[j,j] = cost.Q[i]
+    end
+end
+
+toquat(::Type{<:UnitQuaternion}, q::AbstractVector) = q
+toquat(::Type{Rot}, q::AbstractVector) where Rot <: Rotation = 
+    Rotations.params(UnitQuaternion(Rot(q)))
+
+
+# Jacobians missing from Rotatations
+function Rotations.jacobian(::Type{UnitQuaternion}, p::RodriguesParam)
+    p = Rotations.params(p)
+    np = p'p 
+    d = 1/sqrt(1 + p'p)
+    d3 = d*d*d
+    ds = -d3 * p
+    pp = -d3*(p*p')
+    SA[
+        ds[1] ds[2] ds[3];
+        pp[1] + d pp[4] pp[7]; 
+        pp[2] pp[5] + d pp[8];
+        pp[3] pp[6] pp[9] + d;
+    ]
+end
+
+function Rotations.jacobian(::Type{RodriguesParam}, q::UnitQuaternion)
+    s = 1/q.w
+    SA[
+        -s*s*q.x s 0 0;
+        -s*s*q.y 0 s 0;
+        -s*s*q.z 0 0 s
+    ]
+end
+
+Rotations.jacobian(::Type{R}, q::R) where R = I
--- test/quatcosts.jl
+++ test/quatcosts.jl
@@ -8,116 +8,44 @@ using Rotations
 using StaticArrays, LinearAlgebra, ForwardDiff
 const TO = TrajectoryOptimization
 
-for rot in (:UnitQuaternion, :MRP, :RodriguesParam)
-    @eval rotation_name(::Type{<:$rot}) = $rot
-end
+import TrajectoryOptimization: DiagonalLieCost 
 
-struct DiagonalLieCost{n,m,T,nV,nR,Rot} <: TO.QuadraticCostFunction{n,m,T}
-    Q::SVector{nV,T}
-    R::Diagonal{T,SVector{m,T}}
-    q::SVector{nV,T}
-    r::SVector{m,T}
-    c::T
-    w::Vector{T}                     # weights on rotations (1 per rotation)
-    vinds::SVector{nV,Int}           # inds of vector states
-    qinds::Vector{SVector{nR,Int}}   # inds of rot states
-    qrefs::Vector{UnitQuaternion{T}} # reference rotations
-    function DiagonalLieCost(s::LieState{Rot,P},
-            Q::AbstractVector{<:Real}, R::AbstractVector{<:Real}, 
-            q::AbstractVector{<:Real}, r::AbstractVector{<:Real},
-            c::Real, w::AbstractVector,
-            qrefs::Vector{<:Rotation}
-        ) where {Rot,P}
-        n = length(s)
-        m = length(R)
-        nV = sum(P)
-        nR = Rotations.params(Rot)
-        num_rots = length(P)-1
-        @assert length(Q) == length(q) == nV
-        @assert length(r) == m 
-        @assert length(qrefs) == length(w) == num_rots
-        vinds = [RobotDynamics.vec_inds(Rot, P, i) for i = 1:num_rots+1]
-        rinds = [RobotDynamics.rot_inds(Rot, P, i) for i = 1:num_rots]
-        vinds = SVector{nV}(vcat(vinds...))
-        rinds = SVector{nR}.(rinds)
-        qrefs = UnitQuaternion.(qrefs)
-        T = promote_type(eltype(Q), eltype(R), eltype(q), eltype(r), typeof(c), eltype(w))
-        R = Diagonal(SVector{m}(R))
-        new{n,m,T,nV,nR,rotation_name(Rot)}(Q, R, q, r, c, w, vinds, rinds, qrefs)
-    end
-end
-Union{<:Diagonal{Tr},<:AbstractVector{Tr}} where {Tr}
 
-function DiagonalLieCost(s::LieState{Rot,P}, 
-        Q::Vector{<:AbstractVector{Tq}}, R::Union{<:Diagonal{Tr},<:AbstractVector{Tr}};
-        q = [zeros(Tq,p) for p in P], r=zeros(Tr,size(R,1)), 
-        c=0.0, w=ones(RobotDynamics.num_rotations(s)),
-        qrefs=[one(UnitQuaternion) for i in 1:RobotDynamics.num_rotations(s)]
-    ) where {Rot,P,Tq,Tr}
-    @assert length.(Q) == collect(P) == length.(q)
-end
+
+## Test constructors
 P = (3,6)
 Qs = [rand(p) for p in P]
 s = LieState(UnitQuaternion,3,6)
 R = rand(4)
-DiagonalLieCost(s, Qs, R)
+dcost = DiagonalLieCost(s, Qs, R)
+@test dcost.w == [1]
+@test dcost.Q == [Qs[1]; Qs[2]]
+@test length(dcost.Q) == sum(P)
 
-RobotDynamics.rotation_type(::DiagonalLieCost{<:Any,<:Any,<:Any, <:Any,<:Any, Rot}) where Rot = Rot
-RobotDynamics.state_dim(::DiagonalLieCost{n}) where n = n
-RobotDynamics.control_dim(::DiagonalLieCost{<:Any,m}) where m = m
-TO.is_blockdiag(::DiagonalLieCost) = true
-TO.is_diag(::DiagonalLieCost) = true
+# Pass in Diagonal R
+dcost = DiagonalLieCost(s, Qs, Diagonal(R))
+@test dcost.R.diag == R
 
-function TO.stage_cost(cost::DiagonalLieCost, x::AbstractVector)
-    Rot = RobotDynamics.rotation_type(cost)
-    Jv = veccost(cost.Q, cost.q, x, cost.vinds)
-    Jr = quatcost(Rot, cost.w, x, cost.qinds, cost.qrefs)
-    return Jv + Jr
-end
+# Pass in Diagonal Q for vector parts
+Q = Diagonal(vcat(Qs...))
+q = rand(9)
+dcost = DiagonalLieCost(s, Q, Diagonal(R), q=q, w=[5])
+@test dcost.Q == [Qs[1]; Qs[2]]
+@test dcost.q == q 
+@test dcost.w[1] === 5.0
 
-function veccost(Q, q, x, vinds)
-    xv = x[vinds]
-    0.5*xv'Diagonal(Q)*xv + q'xv
-end
+# Pass in Diagonal Q for all states
+Q = Diagonal(rand(13))
+q = rand(13)
+dcost = DiagonalLieCost(s, Q, Diagonal(R), q=q)
+@test dcost.Q == [Q[1:3]; Q[8:13]]
+@test dcost.q == [q[1:3]; q[8:13]]
+@test dcost.w[1] ≈ sum(Q[4:7])
 
-function quatcost(::Type{Rot}, w, x, qinds, qref) where Rot<:Rotation
-    J = zero(eltype(x))
-    for i = 1:length(qinds) 
-        # q = Rotations.params(UnitQuaternion(Rot(x[qinds[i]])))
-        q = toquat(Rot, x[qinds[i]])
-        qd = Rotations.params(qref[i])
-        err = q'qd
-        J = w[i]*min(1-err,1+err)
-    end
-    return J
-end
+# Pass in weight, overriding default behavior of summing diagonal elements
+dcost = DiagonalLieCost(s, Q, Diagonal(R), q=q, w=[6])
+@test dcost.w[1] === 6.0
 
-function TO.gradient!(E::TO.QuadraticCostFunction, cost::DiagonalLieCost, x::AbstractVector)
-    # Vector states
-    Rot = RobotDynamics.rotation_type(cost)
-    xv = x[cost.vinds]
-    E.q[cost.vinds] .= cost.Q .* xv + cost.q
-
-    # Quaternion states
-    for i = 1:length(cost.qinds)
-        qind = cost.qinds[i]
-        q = toquat(Rot, x[qind])
-        qref = Rotations.params(cost.qrefs[i])
-        dq = q'qref
-        E.q[qind] .= -cost.w[i]*qref*sign(dq)
-    end
-    return false
-end
-
-function TO.hessian!(E::TO.QuadraticCostFunction, cost::DiagonalLieCost{n}, x::AbstractVector) where n
-    for (i,j) in enumerate(cost.vinds) 
-        E.Q[j,j] = cost.Q[i]
-    end
-end
-
-toquat(::Type{<:UnitQuaternion}, q::AbstractVector) = q
-toquat(::Type{Rot}, q::AbstractVector) where Rot <: Rotation = 
-    Rotations.params(UnitQuaternion(Rot(q),false))
 
 ## Set up
 model = Quadrotor{UnitQuaternion}()
@@ -133,6 +61,7 @@ qrefs = [rand(UnitQuaternion) for i = 1:RobotDynamics.num_rotations(s)]
 
 ## Call inner constructor
 costfun = DiagonalLieCost(s, Q, R, q, r, c, w, qrefs)
+
 
 # Test stage cost
 TO.stage_cost(costfun, x)
@@ -171,3 +100,49 @@ E.Q .*= 0
 @test TO.hessian!(E, costfun, x, u) == true
 @test E.Q ≈ hess
 @test E.R ≈ Diagonal(R)
+
+
+
+## Try with an MRP
+s1 = LieState(UnitQuaternion,P)
+s2 = LieState(MRP,P)
+dcost1 = DiagonalLieCost(s1, Qs, R)
+dcost2 = DiagonalLieCost(s2, Qs, R)
+model1 = Quadrotor{UnitQuaternion}()
+model2 = Quadrotor{MRP}()
+@test dcost2.Q == vcat(Qs...)
+@test state_dim(dcost1) == 13
+@test state_dim(dcost2) == 12
+@test dcost2.vinds[end] == 12
+@test dcost2.qinds[1] == [4,5,6]
+u = @SVector rand(4)
+x = rand(RBState)
+x1 = RobotDynamics.build_state(model1, x)
+x2 = RobotDynamics.build_state(model2, x)
+
+@test TO.stage_cost(dcost1, x1, u) ≈ TO.stage_cost(dcost2, x2, u)
+
+## Test conversion jacobians
+q = rand(UnitQuaternion)
+
+quat2mrp(q) = Rotations.params(MRP(UnitQuaternion(q)))
+mrp2quat(g) = Rotations.params(UnitQuaternion(MRP(g)))
+rp2quat(p) = Rotations.params(UnitQuaternion(RodriguesParam(p)))
+quat2rp(q) = Rotations.params(RodriguesParam(UnitQuaternion(q)))
+
+Rotations.jacobian(UnitQuaternion, MRP(q)) ≈ 
+    ForwardDiff.jacobian(mrp2quat, Rotations.params(MRP(q)))
+Rotations.jacobian(MRP, q) ≈ 
+    ForwardDiff.jacobian(quat2mrp, Rotations.params(q))
+
+Rotations.jacobian(UnitQuaternion, RodriguesParam(q)) ≈ 
+    ForwardDiff.jacobian(rp2quat, Rotations.params(RodriguesParam(q)))
+
+@test Rotations.jacobian(RodriguesParam, q) ≈
+    ForwardDiff.jacobian(quat2rp, Rotations.params(q))
+
+@btime Rotations.jacobian(UnitQuaternion, RodriguesParam($q))
+
+g = Rotations.params(RodriguesParam(q))
+rp2quat(g)
+1/sqrt(1+g'g) * [1; g]
